7f3f068050377faa7f31f6a0d27074907b9ac27f
Marcel Petrick <mail@marcelpetrick.it>
2018-06-06 17:23:22 +0200

    day8: working tree-constructor; working "value first"-flattening

--- dailyCodingTask/day8_countUnivalueNodes.py
+++ dailyCodingTask/day8_countUnivalueNodes.py
@@ -18,38 +18,58 @@ import unittest
 
 # a tree consists of nodes with up to two children.
 
-class TreeNode:
-    value = 0
-    left = None
-    right = None
+#------------------------------------------------------------------------------
 
+class TreeNode:
     def __init__(self, value = -1, leftNode = None, rightNode = None):
+        # todo assert that value is just (0,1) - else throw an exception
         self.value = value
         self.leftNode = leftNode
         self.rightNode = rightNode
 
     def isUnival(self):
         # todo: not complete: it is not enough that both children are unival, but also have the same value!
-        return (self.leftNode is None) AND (self.rightNode is None) # OR (self.leftNode.isUnival() AND self.rightNode.isUnival()))
-        #return True
+        #return (self.leftNode is None) AND (self.rightNode is None) # OR (self.leftNode.isUnival() AND self.rightNode.isUnival()))
+        return True
 
+    def getTreeString(self):
+        ''' recursive iteration '''
+        returnValue = str(self.value)
 
-class ProductListTestCase(unittest.TestCase):
+        if self.leftNode is not None:
+            returnValue += "(" + self.leftNode.getTreeString() + ")"
+
+        if self.rightNode is not None:
+            returnValue += "[" + self.rightNode.getTreeString() + "]"
+
+        #print(returnValue)
+        return returnValue
+
+#------------------------------------------------------------------------------
+
+class UnivalTestcase(unittest.TestCase):
     ''' Tests for day8.py '''
 
     def testUnivalDetection0(self):
-        inputTree = TreeNode(1, None, None)
+        inputTree = TreeNode(1)
         self.assertTrue(inputTree)
 #        print("countDecodePossibilities(" + inputString + ") = " + str(countDecodePossibilities(inputString)))
 
-    def testUnivalDetection1(self):
-        inputTree = TreeNode(1, TreeNode(0, None, None), None)
-        self.assertFalse(inputTree)
+    # def testUnivalDetection1(self):
+    #     inputTree = TreeNode(1, TreeNode(0, None, None), None)
+    #     self.assertFalse(inputTree)
+    #
+    # def testUnivalDetection2(self):
+    #     inputTree = TreeNode(1, None, TreeNode(0, None, None))
+    #     self.assertFalse(inputTree)
 
-    def testUnivalDetection2(self):
-        inputTree = TreeNode(1, None, TreeNode(0, None, None))
-        self.assertFalse(inputTree)
+#------------------------------------------------------------------------------
 
 # ---- here comes the execution of the unit-tests ----
-if __name__ == '__main__':
-    unittest.main()
+#if __name__ == '__main__':
+#    unittest.main()
+
+
+print("output:", TreeNode(1).getTreeString())
+
+print("output:", TreeNode(0, TreeNode(1), TreeNode(2)).getTreeString())
