ccbac87d52eeb9e8c190709384917a47a51d9ed5
Marcel Petrick <mail@marcelpetrick.it>
2018-07-26 18:01:32 +0200

    day58: after lots of thinking some external spark is needed

--- dailyCodingTask/day58_findInRotatedArray.py
+++ dailyCodingTask/day58_findInRotatedArray.py
@@ -8,32 +8,53 @@
 #
 # You can assume all the integers in the array are unique.
 
-
-
-
-###################
-
+############################# help needed from https://www.geeksforgeeks.org/search-an-element-in-a-sorted-and-pivoted-array/ #############################
+# 1) Find middle point mid = (l + h)/2
+# 2) If key is present at middle point, return mid.
+# 3) Else If arr[l..mid] is sorted
+#     a) If key to be searched lies in range from arr[l]
+#      to arr[mid], recur for arr[l..mid].
+#     b) Else recur for arr[mid+1..r]
+# 4) Else (arr[mid+1..r] must be sorted)
+#     a) If key to be searched lies in range from arr[mid+1]
+#        to arr[r], recur for arr[mid+1..r].
+#     b) Else recur for arr[l..mid]
+##########################################################
 
 import unittest
 
+# ------------------------------------------------------------------------------
 
 def findInRotatedArray(inputList, element):
     print("findInRotatedArray called with: ", inputList, " and element ", element)
-    returnValue = -1 # is the found index
+    returnValue = 0 # this will be the found index; zero by default and if not found (weird: shall be different than index == 0!)
+
+    return returnValue
+
+
+def findPivotElement(inputList):
+    ''' Used to find the pivot element in the given list'''
+    returnValue = 0
+    if inputList.__len__() > 0:
+        # returnValue = inputList[0]
+
+        first = inputList[0]
+        last = inputList[inputList.__len__() - 1]
+        middle = inputList[inputList.__len__() / 2]
 
     return returnValue
 
 # ------------------------------------------------------------------------------
 
-class UnivalTestcase(unittest.TestCase):
-    ''' Tests for day8_countUnivalSubtrees.py '''
+class Testcase(unittest.TestCase):
+    ''' Tests for day58_findInRotatedArray.py '''
 
     def test0(self):
         inputList = [13, 18, 25, 2, 8, 10]
         element = 8
         expectedResult = 4
         output = findInRotatedArray(inputList, element)
-        self.assertEqual(output , expectedResult)
+        self.assertEqual(output, expectedResult)
         print(" --> input", inputList, " with element ", element, "yielded result:", output, ":)")
 
 # ------------------------------------------------------------------------------
