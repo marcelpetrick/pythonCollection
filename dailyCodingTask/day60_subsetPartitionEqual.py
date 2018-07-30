edd2924ee74dd702ec23e36f93a6f0881d7904bc
Marcel Petrick <mail@marcelpetrick.it>
2018-07-30 17:05:52 +0200

    day60: some debugging for the "potenzmenge"; and it works!

--- dailyCodingTask/day60_subsetPartitionEqual.py
+++ dailyCodingTask/day60_subsetPartitionEqual.py
@@ -10,6 +10,7 @@
 # subsets that add up to the same sum.
 
 import unittest
+import copy
 
 # ------------------------------------------------------------------------------
 
@@ -20,7 +21,9 @@ def partitionIntoEqualSumMultiset(multiset):
     print("partitionIntoEqualSumMultiset called with: ", multiset)
     returnValue = False
 
-    generateAllSubsetsForSet(multiset)
+    output = generateAllSubsetsForSet(multiset)
+
+    print("final set:", output)
 
     return returnValue
 
@@ -28,20 +31,29 @@ def partitionIntoEqualSumMultiset(multiset):
 
 def generateAllSubsetsForSet(multiset): # input-param multiset is a list
     ''' Generate all possible sets for the given list. '''
-    resultSet = list() # set of lists or list of lists?
-    # decision: set of lists!
+
     print("generateAllSubsetsForSet called with: ", multiset)
 
+    resultSet = list() # set of lists or list of lists?
+    resultSet.append([])
+
     for elem in multiset:
-        resultSet.append([])
         print("up:", resultSet, " ::", len(resultSet))
-        #copy = resultSet.copy()
+        #original = list(resultSet) # the original content has to be retained
+        original = copy.deepcopy(resultSet) # ATTENTION: just this works, not just cloning or copying the list (of lists) ..
+        print("original has x elems: ", original.__len__(), ":", original)
+
         for index in range(len(resultSet)):
             print("current elem:", resultSet[index])
-            resultSet[index].append(elem)
-        #resultSet += copy
-        print("down:", resultSet, " ::", len(resultSet))
+            resultSet[index].append(elem) # immediately change the items
+            print("\t after change elem:", resultSet[index])
 
+        print("original has x elems: ", original.__len__(), ":", original)
+        print("resultlist has x elems: ", resultSet.__len__(), ":", resultSet)
+
+        resultSet = resultSet + original # add the copied initial list
+        print("appended version has x elems: ", resultSet.__len__(), "--->", resultSet)
+        print("----------------------")
 
     return resultSet
 
@@ -68,5 +80,6 @@ def generateAllSubsetsForSet(multiset): # input-param multiset is a list
 # if __name__ == '__main__':
 #     unittest.main()
 
-multiset = [1, 2, 3, 4]
+multiset = [1, 2, 3]
 partitionIntoEqualSumMultiset(multiset)
+# expected: [[1, 2, 3], [2, 3], [1, 3], [3], [1, 2], [2], [1], []]
