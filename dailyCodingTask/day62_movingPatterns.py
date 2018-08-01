2b4c7906e45dca589525ffbfddeb48be6e3263e1
Marcel Petrick <mail@marcelpetrick.it>
2018-08-01 17:25:49 +0200

    day62: fixed algorithm; now the recursion works (if there is just one path, then just add 1. no recursive call!)

--- dailyCodingTask/day62_movingPatterns.py
+++ dailyCodingTask/day62_movingPatterns.py
@@ -18,34 +18,17 @@ def computeWaysToMove(m, n):
     :param m - one side of the matrix
     :param m - other side of the matrix
     '''
-    print("computeWaysToMove called with: ", m, n)
+    
     returnValue = None
 
     # todo prevent nonsense for wrong input, like negative numbers
 
     # positions get enumerated with 1..n - not starting with 0
-    returnValue = computeWaysToMoveWithPosition(m, n, 1, 1) # start at top-left
-
-    return returnValue
-
-def computeWaysToMoveWithPosition(m, n, x, y):
-    ''' helper with current position '''
-    returnValue = None
-
-    # compute the amount of possible solutions for both possible ways (horizontal and vertical) recursively; sum them up
-    if x < m:
-        horizontalSolutions = 1 + computeWaysToMoveWithPosition(m, n, x+1, y)
+    if(m == 1 or n == 1):
+        returnValue = 1  # because you can just follow all the way horizontal or vertical
     else:
-        horizontalSolutions = 0
+        returnValue = computeWaysToMove(m - 1, n) + computeWaysToMove(m, n - 1)
 
-    if y < n:
-        verticalSolutions = 1 + computeWaysToMoveWithPosition(m, n, x, y+1)
-    else:
-        verticalSolutions = 0
-
-    returnValue = horizontalSolutions + verticalSolutions
-
-    print("computeWaysToMoveWithPosition called with: ", m, n, x, y, "and will return:", returnValue)
     return returnValue
 
 # ------------------------------------------------------------------------------
@@ -54,6 +37,20 @@ class Testcase(unittest.TestCase):
     def test0(self):
         m, n = 2,2
         expectedResult = 2
+        output = computeWaysToMove(m, n)
+        self.assertEqual(output, expectedResult)
+        print(" --> input", m, "x", n, "yielded result:", output)
+
+    def test1(self):
+        m, n = 3,3
+        expectedResult = 6
+        output = computeWaysToMove(m, n)
+        self.assertEqual(output, expectedResult)
+        print(" --> input", m, "x", n, "yielded result:", output)
+
+    def test2(self):
+        m, n = 5,5
+        expectedResult = 70
         output = computeWaysToMove(m, n)
         self.assertEqual(output, expectedResult)
         print(" --> input", m, "x", n, "yielded result:", output)
