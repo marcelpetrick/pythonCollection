08bf6ec3c85ca598036babcc89135963daca41cb
Marcel Petrick <mail@marcelpetrick.it>
2018-08-02 10:04:38 +0200

    stepping numbers: updated the task

--- dailyCodingTask/steppingNumbers.py
+++ dailyCodingTask/steppingNumbers.py
@@ -1,3 +1,14 @@
+# Stepping Numbers
+# Given two integers ‘n’ and ‘m’, find all the stepping numbers in range [n, m].
+# A number is called stepping number if all adjacent digits have an absolute difference of 1. 321 is a Stepping Number while 421 is not.
+#
+# Examples :
+# Input : n = 0, m = 21
+# Output : 0 1 2 3 4 5 6 7 8 9 10 12 21
+#
+# Input : n = 10, m = 15
+# Output : 10, 12
+
 def printSteppingNumbersInRange(m, n):
     '''
     @param: m begin (inclusive)
