25f2605d6dc00a817df1e66ec13f00a473edc4cf
Marcel Petrick <mail@marcelpetrick.it>
2018-08-06 15:04:16 +0200

    day66: unbiased coin-throwing from biased one; working implementation

--- dailyCodingTask/day66_biasedCoin.py
+++ dailyCodingTask/day66_biasedCoin.py
@@ -19,6 +19,7 @@ def biasedCoin():
     return (value >= 60)
 
 
+# test-programm: probability should be around 0.4
 cointhrows = 1000000
 
 probability = 0
@@ -26,5 +27,23 @@ for x in range(cointhrows):
     if biasedCoin():
         probability += 1
 
-print(probability / cointhrows)
+print("biased coin after", cointhrows ,"throws -->", probability / cointhrows)
 
+# ------------------------------------------------------------------------------
+
+def unbiasedCoin():
+    coin0, coin1 = biasedCoin(), biasedCoin()
+    while coin0 == coin1:
+        #print("throw")
+        coin0, coin1 = biasedCoin(), biasedCoin()
+    return coin0 # just pick one; could be as well the second one
+
+probability = 0
+for x in range(cointhrows):
+    if unbiasedCoin():
+        probability += 1
+
+print("unbiased coin after", cointhrows ,"throws -->", probability / cointhrows)
+
+# a very good explanation why this works can be found at https://jeremykun.com/2014/02/08/simulating-a-fair-coin-with-a-biased-coin/
+# (similar: http://yilinmo.github.io/fair-results-from-a-biased-coin)
