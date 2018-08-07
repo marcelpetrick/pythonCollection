e74760adda2b011fe3d13285858a89b6b32ee682
Marcel Petrick <mail@marcelpetrick.it>
2018-08-07 12:02:44 +0200

    day69: task added; also added my "unittest-framework"

---
+++ dailyCodingTask/day69_biggestProduct.py
@@ -0,0 +1,36 @@
+# This problem was asked by Facebook.
+#
+# Given a list of integers, return the largest product that can be made by multiplying any three integers.
+#
+# For example, if the list is [-10, -10, 5, 2], we should return 500, since that's -10 * -10 * 5.
+#
+# You can assume the list has at least three integers.
+
+import unittest
+
+# ------------------------------------------------------------------------------
+
+def biggestProduct(inputList):
+    print("biggestProduct called with: ", inputList)
+    returnValue = None
+
+    return returnValue
+
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+
+class Testcase(unittest.TestCase):
+    def test0(self):
+        inputList = [-10, -10, 5, 2]
+        expectedResult = 500
+        output = biggestProduct(inputList)
+        self.assertEqual(output, expectedResult)
+        print(" --> input", inputList, "yielded result:", output)
+
+
+# # ------------------------------------------------------------------------------
+#
+# ---- here comes the execution of the unit-tests ----
+if __name__ == '__main__':
+    unittest.main()
