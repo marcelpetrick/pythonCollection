5d3337f6d416f14d31798b08ac7601e0616bca21
Marcel Petrick <mail@marcelpetrick.it>
2018-08-07 18:00:20 +0200

    day69: started implemenation of the biggest pos/biggest neg (in absolute terms)-container.

--- dailyCodingTask/day69_biggestProduct.py
+++ dailyCodingTask/day69_biggestProduct.py
@@ -6,16 +6,75 @@
 #
 # You can assume the list has at least three integers.
 
-# my idea: go once throught the whole list, check each element with container.
+# my idea: go once through the whole list, check each element with container.
 # that container saves the two smallest negative numbers and the three biggest.
 # afterwards: choose wisely ;)
+# if the product of the two negative numbers (can either be 0 or 2 negative numbers, because with one or three negative
+# ones the product becomes negative -> fail.
 
 import unittest
 
 # ------------------------------------------------------------------------------
 
+class ContainerForBiggestAbs: #
+    ''' idea is to push all incoming data to the corresponding list (neg or pos). Then do a cutoff after sorting. '''
+    negativeList = []
+    positiveList = []
+    negativeAmount = 2 # constant
+    positiveAmount = 3 # constant
+
+    def __init__(self):
+        # nothing to do here
+        print("Lexer::__init__")
+
+    def check(self, value):
+        ''' Check the given value and sort it into fitting lists.
+        The one for negative numbers inverts them automatically, because it will be only two elemnts long.
+        '''
+        if value == 0:
+            return # we don't need ya!
+
+        # quite copy&paste-d code. Could be improved.
+        # todo unit tests!
+        if value < 0:
+            self.negativeList.append(-value) # negate!
+            self.negativeList.sort()
+            self.negativeList.reverse() # or sort reverted ....
+            while self.negativeList.__len__() > self.negativeAmount:
+                self.negativeList.pop()
+            #print("neg: after pop", self.negativeList)
+        else: # must be 0 or positive
+            self.positiveList.append(value) # negate!
+            self.positiveList.sort()
+            self.positiveList.reverse() # or sort reverted ....
+            while self.positiveList.__len__() > self.positiveAmount:
+                self.positiveList.pop()
+            #print("pos: after pop", self.positiveList)
+
+    def getNegativeList(self):
+        ''' Check as well if we have an even number of negative values. '''
+        amount = len(self.negativeList)
+        if amount == 0 or amount == 2:
+            return self.negativeList
+        else:
+            return []
+
+    def getPositiveList(self):
+        return self.positiveList
+
+# ------------------------------------------------------------------------------
+
 def biggestProduct(inputList):
     print("biggestProduct called with: ", inputList)
+
+    theContainer = ContainerForBiggestAbs()
+    # iterate over the whole input
+    for elem in inputList:
+        theContainer.check(elem)
+
+    print("biggestProduct: pos:", theContainer.getPositiveList())
+    print("biggestProduct: neg:", theContainer.getNegativeList())
+
     returnValue = None
 
     return returnValue
@@ -38,3 +97,8 @@ class Testcase(unittest.TestCase):
 # ---- here comes the execution of the unit-tests ----
 if __name__ == '__main__':
     unittest.main()
+
+
+print("###############################")
+inputList = [-5, -3, -4, -1, 90, 30, -2, -6, 80, 20, 100]
+output = biggestProduct(inputList)
