54ea2b48c7f788e1e70807a8e1537409b142dc1a
Marcel Petrick <mail@marcelpetrick.it>
2018-08-10 18:02:44 +0200

    day71: simple solution by cut-off (todo: has to be checked!)

--- dailyCodingTask/day71_rand5Fromrand7.py
+++ dailyCodingTask/day71_rand5Fromrand7.py
@@ -2,3 +2,27 @@
 #
 # Using a function rand7() that returns an integer from 1 to 7 (inclusive)
 # with uniform probability, implement a function rand5() that returns an integer from 1 to 5 (inclusive).
+
+import random
+
+# given
+def rand7():
+    return random.randint(1, 7)
+
+# to implement: idea: just drop all values bigger than 5.
+# todo proof statistically that this is really the case
+def rand5():
+    result = rand7()
+    while result > 5:
+        result = rand7()
+
+    return result
+
+# ---------- test -----------------
+
+#for _ in range(10):
+#    print(rand7())
+
+
+for _ in range(100):
+    print(rand5())
