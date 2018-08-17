528071efb66875acb2118cac2d3483f33b61973d
Marcel Petrick <mail@marcelpetrick.it>
2018-08-17 15:54:43 +0200

    problem003: working implementation

--- ProjectEuler/problem003_largestPrimeFactor.py
+++ ProjectEuler/problem003_largestPrimeFactor.py
@@ -5,17 +5,26 @@
 # ------------------------------------------------------------------------------
 import unittest
 
-# idea: (of course, not the most efficient way to fix it)
-def findLargestPrimeFactor():
-    pass
+# idea:
+# (of course, not the most efficient way to fix it) decrease the number of the input to find a fitting divisor
+# (% == 0), then check if this is prime
+def findLargestPrimeFactor(input):
+    for decreasingValue in range(input, 1, -1):
+        if input % decreasingValue == 0:
+            #print(decreasingValue, "divides", input, "without remainder: test now for prime")
+            if isPrime(decreasingValue):
+                #print("is prime - nice :) return this")
+                return decreasingValue
+
+    return -1 # error
 
 # ------------------------------------------------------------------------------
 
 def isPrime(input):
     for x in range(2, input): # is -1
-        print("input:", input, "test:", x)
+        #print("input:", input, "test:", x)
         if input % x == 0:
-            print("is dividing!")
+            #print("is dividing!")
             return False
     # else we have a prime if we reached this
     return True
@@ -34,10 +43,15 @@ class Testcase(unittest.TestCase):
         self.assertEqual(False, isPrime(87))  # true?
         self.assertEqual(True, isPrime(89))  # true?
 
-    # def test_findLargetPrimeFactor(self):
-    #     input = 13195
-    #     expectedResult = 29
-    #     output = findLargestPrimeFactor(input)
-    #     self.assertEqual(output, expectedResult)
-    #     print(" --> input", input, "yielded result:", output)
+    def test_findLargetPrimeFactor(self):
+        input = 13195
+        expectedResult = 29
+        output = findLargestPrimeFactor(input)
+        self.assertEqual(output, expectedResult)
+        print(" --> input", input, "yielded result:", output)
 
+
+print(2, findLargestPrimeFactor(2))
+print(6, findLargestPrimeFactor(6))
+print(25, findLargestPrimeFactor(25))
+print(87, findLargestPrimeFactor(87))
