a6fe837c6b78845ae10a97aa39664746cd767970
Marcel Petrick <mail@marcelpetrick.it>
2018-08-17 15:35:51 +0200

    problem003: euler: find largest prime factor

---
+++ ProjectEuler/problem003_largestPrimeFactor.py
@@ -0,0 +1,9 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+#
+# What is the largest prime factor of the number 600851475143 ?
+
+# ------------------------------------------------------------------------------
+
+# idea: (of course, not the most efficient way to fix it)
+def findLargestPrimeFactor():
+    pass
