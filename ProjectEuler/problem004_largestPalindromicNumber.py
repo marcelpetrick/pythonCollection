1dcd8862841e039cbd8d36a752dbdd0fb22ea4c7
Marcel Petrick <mail@marcelpetrick.it>
2018-08-17 17:10:22 +0200

    problem004: added unit-test and some helper functions

--- ProjectEuler/problem004_largestPalindromicNumber.py
+++ ProjectEuler/problem004_largestPalindromicNumber.py
@@ -4,6 +4,8 @@
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
 # ------------------------------------------------------------------------------
+import unittest
+# ------------------------------------------------------------------------------
 
 def findLargestPalindromicNumberFromTwoXDigitNumbers(digits):
     pass
@@ -11,7 +13,25 @@ def findLargestPalindromicNumberFromTwoXDigitNumbers(digits):
 # ------------------------------------------------------------------------------
 
 def isPalindrom(input):
-    pass
+    if input < 0: # all negative numbers are not palindromic
+        return False
+    elif input < 10: # all single digit numbers are palindromic
+        return True
+    else: # first is equal the last digit? then check for the inner core
+        return None
+
+# ------------------------------------------------------------------------------
+
+class Testcase(unittest.TestCase):
+    def test_isPalindrom(self):
+        self.assertEqual(True, isPalindrom(1))
+        self.assertEqual(True, isPalindrom(11))
+        self.assertEqual(False, isPalindrom(12))
+        self.assertEqual(True, isPalindrom(121))
+        self.assertEqual(True, isPalindrom(1221))
+        self.assertEqual(False, isPalindrom(123421))
+        self.assertEqual(True, isPalindrom(123321))
+        self.assertEqual(False, isPalindrom(-2))
 
 # ------------------------------------------------------------------------------
 
