d368d73b7a09a8192c2ce8c58bea773a99ceb358
Marcel Petrick <mail@marcelpetrick.it>
2018-08-17 17:53:24 +0200

    problem004: fixed the palindrom-recognition

--- ProjectEuler/problem004_largestPalindromicNumber.py
+++ ProjectEuler/problem004_largestPalindromicNumber.py
@@ -7,23 +7,40 @@
 import unittest
 # ------------------------------------------------------------------------------
 
+# idea: try to check downwards: use two loops over the input; starting with the biggest possible number from the amount of digits
 def findLargestPalindromicNumberFromTwoXDigitNumbers(digits):
     pass
 
 # ------------------------------------------------------------------------------
 
+# todo rename the function name: e at suffix missing
 def isPalindrom(input):
+    #print("-----------------------")
+    #print("isafdasf called with", input)
     if input < 0: # all negative numbers are not palindromic
         return False
     elif input < 10: # all single digit numbers are palindromic
         return True
     else: # first is equal the last digit? then check for the inner core
-        return None
+        stringifiedNumber = str(input)
+        #print(stringifiedNumber[0], stringifiedNumber[-1]) # -1 is last elem
+        if stringifiedNumber[0] == stringifiedNumber[-1]:
+            #print("same!")
+            substring = stringifiedNumber[1:-1]
+            if substring.__len__() > 0:
+                #print("call with substring", substring, "the function")
+                return isPalindrom(int(substring))
+            else:
+                #print("no more stuff left - return True")
+                return True
+        else:
+            #print("begin and end not equal :'(")
+            return False
 
 # ------------------------------------------------------------------------------
 
 class Testcase(unittest.TestCase):
-    def test_isPalindrom(self):
+    def test_isPalindrome(self):
         self.assertEqual(True, isPalindrom(1))
         self.assertEqual(True, isPalindrom(11))
         self.assertEqual(False, isPalindrom(12))
@@ -33,7 +50,14 @@ class Testcase(unittest.TestCase):
         self.assertEqual(True, isPalindrom(123321))
         self.assertEqual(False, isPalindrom(-2))
 
+    def test_largestPalindromic(self):
+        print("dsfsafsadf")
+        self.assertEqual(9009, findLargestPalindromicNumberFromTwoXDigitNumbers(2))
+
 # ------------------------------------------------------------------------------
 
 # should return 91x99 --> 9009
-findLargestPalindromicNumberFromTwoXDigitNumbers(2)
+#findLargestPalindromicNumberFromTwoXDigitNumbers(2)
+
+#print("##################################")
+#isPalindrom(123421)
