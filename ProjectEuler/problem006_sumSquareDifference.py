a7a446e88dcaddfd9a8ed149890913be690c53e3
Marcel Petrick <mail@marcelpetrick.it>
2018-08-20 18:04:08 +0200

    problem006: working solution

--- ProjectEuler/problem006_sumSquareDifference.py
+++ ProjectEuler/problem006_sumSquareDifference.py
@@ -9,3 +9,32 @@
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
 # ------------------------------------------------------------------------------
+
+def sumSquareDifference(number):
+
+    listList = list(range(1, number+1))
+    sumOfSquares = sum([elem ** 2 for elem in listList])
+    squareOfSum = sum(listList) ** 2
+    #print(sumOfSquares)
+    #print(squareOfSum)
+    print("result:", squareOfSum - sumOfSquares)
+
+# ------------------------------------------------------------------------------
+
+sumSquareDifference(10)
+sumSquareDifference(100)
+
+# ------------------------------------------------------------------------------
+
+# result: 2640
+# result: 25164150
+
+# Sorry, no unit-testing for this. Not sure why it was a "problem" at all!
+
+# ------------------------------------------------------------------------------
+
+# Congratulations, the answer you gave to problem 6 is correct.
+#
+# You are the 406105th person to have solved this problem.
+#
+# This problem had a difficulty rating of 5%. The highest difficulty rating you have solved remains at 5%.
