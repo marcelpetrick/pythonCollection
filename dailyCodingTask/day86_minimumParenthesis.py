a8eb64524facdfa9a9ddbdd00892cea4a0df9f0a
Marcel Petrick <mail@marcelpetrick.it>
2018-08-25 23:51:14 +0200

    day86: added the solution-idea and the naked unit-test-framework

--- dailyCodingTask/day86_minimumParenthesis.py
+++ dailyCodingTask/day86_minimumParenthesis.py
@@ -5,3 +5,27 @@
 #
 # For example, given the string "()())()", you should return 1. Given the string ")(", you should return 2,
 # since we must remove all of them.
+
+# ------------------------------------------------------------------------------
+
+# idea:
+# parentheses come in pairs. So a "closed" valid expression has the value 0 if ( stands for +1 and ) for -1.
+# values below zero are not possible: the counter should be then increased by one.
+# additionally after parsing the whole input-string, the remaining (positive) value has to be added to the counter.
+# counter gives then the amount of minimum parentheses to remove
+
+# ------------------------------------------------------------------------------
+import unittest
+# ------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------
+
+class Testcase(unittest.TestCase):
+    pass
+    
+#------------------------------------------------------------------------------
+
+# ---- here comes the execution of the unit-tests ----
+if __name__ == '__main__':
+    unittest.main()
