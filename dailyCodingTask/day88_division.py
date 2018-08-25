747870e2c9c585c44eefea21c77d218aed385fc7
Marcel Petrick <mail@marcelpetrick.it>
2018-08-25 23:40:09 +0200

    day88: implement division without some certain other operators

---
+++ dailyCodingTask/day88_division.py
@@ -0,0 +1,23 @@
+# This question was asked by ContextLogic.
+#
+# Implement division of two positive integers without using the division, multiplication, or modulus operators.
+# Return the quotient as an integer, ignoring the remainder.
+
+# ------------------------------------------------------------------------------
+import unittest
+# ------------------------------------------------------------------------------
+
+def divide(a,b):
+    return a #todo
+
+#------------------------------------------------------------------------------
+
+class Testcase(unittest.TestCase):
+    def test_divide(self):
+        self.assertEqual(1,1)
+
+#------------------------------------------------------------------------------
+
+# ---- here comes the execution of the unit-tests ----
+if __name__ == '__main__':
+    unittest.main()
