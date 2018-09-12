13ac020d8fa079052e9ddb50179506b241f02b0b
Marcel Petrick <mail@marcelpetrick.it>
2018-09-12 18:06:55 +0200

    day105: added task and some ideas

---
+++ dailyCodingTask/day105_debouncedFunction.py
@@ -0,0 +1,23 @@
+# This problem was asked by Facebook.
+#
+# Given a function f, and N return a debounced f of N milliseconds.
+#
+# That is, as long as the debounced f continues to be invoked, f itself will not be called for N milliseconds.
+
+# ------------------------------------------------------------------------------
+
+import unittest
+
+# ------------------------------------------------------------------------------
+
+def bounced(f, n):
+    ''' Plan is to use this as decorator.
+    :param f: the function
+    :param n: the timeout in milliseconds
+    :return: f
+    '''
+    pass
+
+# ------------------------------------------------------------------------------
+
+# add call in a finite loop
