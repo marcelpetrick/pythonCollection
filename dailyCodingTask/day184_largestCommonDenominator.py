4c80d2f09b80d4b5c4a54eadd87bde3b4219b7ec
Marcel Petrick <mail@marcelpetrick.it>
2018-11-30 11:28:17 +0100

    day184: added task; and husk for it
    Change-Id: Ia38d6562592f463dac3b5cee4c40e8f0e71aaadd

---
+++ dailyCodingTask/day184_largestCommonDenominator.py
@@ -0,0 +1,52 @@
+# Good morning! Here's your coding interview problem for today.
+#
+# This problem was asked by Amazon.
+#
+# Given n numbers, find the greatest common denominator between them.
+#
+# For example, given the numbers [42, 56, 14], return 14.
+
+
+# ------------------------------------------------------------------------------
+
+# idea:
+
+# ------------------------------------------------------------------------------
+import unittest
+# ------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+
+class Testcase(unittest.TestCase):
+
+    def test_foo(self):
+        pass
+
+    # def test_computeMinimumNumberOfParenthesesToBeRemoved(self):
+    #     self.assertEqual(0, computeMinimumNumberOfParenthesesToBeRemoved(""))
+    #     self.assertEqual(1, computeMinimumNumberOfParenthesesToBeRemoved("("))
+    #     self.assertEqual(1, computeMinimumNumberOfParenthesesToBeRemoved(")"))
+    #     self.assertEqual(0, computeMinimumNumberOfParenthesesToBeRemoved("()"))
+    #     self.assertEqual(3, computeMinimumNumberOfParenthesesToBeRemoved(")))"))
+    #     self.assertEqual(3, computeMinimumNumberOfParenthesesToBeRemoved("((("))
+    #     # todo add more
+    #
+    # # For example, given the string "()())()", you should return 1.
+    # def test_fromTask0(self):
+    #     self.assertEqual(1, computeMinimumNumberOfParenthesesToBeRemoved("()())()"))
+    #
+    # # Given the string ")(", you should return 2, since we must remove all of them.
+    # def test_fromTask1(self):
+    #     self.assertEqual(2, computeMinimumNumberOfParenthesesToBeRemoved(")("))
+
+#------------------------------------------------------------------------------
+
+# ---- here comes the execution of the unit-tests ----
+if __name__ == '__main__':
+    unittest.main()
+
+
+#------------------------------------------------------------------------------
+print("-------- additional output: begin -------------------------------------------------------------------")
+print("test")
+print("-------- additional output: end ---------------------------------------------------------------------")
