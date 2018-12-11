d13ade7f8dd3bdea1ffc858d946841bb5993f2e3
Marcel Petrick <mail@marcelpetrick.it>
2018-12-11 14:23:55 +0100

    day186: working solution, but ugly code - needs proper unit-test
    Change-Id: I8a2a0dc5fb9e0a1a343b76fec7817d76924eb45d

--- dailyCodingTask/day186_divideSet.py
+++ dailyCodingTask/day186_divideSet.py
@@ -18,16 +18,17 @@ def createPowerset(inputList):
     # question: is the input a seat or a list? does order matter? i think not
 
     result = [[]] # the list which includes at least the empty list
-    for elemInputList in inputList:
-        print("current item:", elemInputList) # todom remove
-        newList = [[]]
+    for elemFromOriginalList in inputList:
+        #print("current item:", elemFromOriginalList) # todom remove
+        newList = [] # totally empty in the beginning
         for elemIntermediateList in result:
-            newItem = elemIntermediateList
-            newItem.append(elemInputList)
+            newItem = elemIntermediateList.copy()
+            newItem.append(elemFromOriginalList)
+            #print("newItem:", newItem)
             newList.append(newItem)
-        print("newList (applied item to old resultList):", newList)
-        result.append(newList)
-        print("current resultlist:", result)
+        #print("newList (applied item to old resultList):", newList)
+        result = result + (newList)
+        #print("current resultlist:", result)
 
     return result
 
@@ -35,11 +36,15 @@ def createPowerset(inputList):
 
 class Testcase(unittest.TestCase):
     def test_createPowerset(self):
-        print("test 1 ")
-        powerSet = createPowerset([1,2,3]) # expected [], [1], [2], [3], [1,2], [1,3], [2,3], [1,2,3] --> 6 elems
-        print(powerSet)
+
+#        powerSet = createPowerset([1,2]) # expected [], [1], [2], [3], [1,2], [1,3], [2,3], [1,2,3] --> 6 elems
+#        print("powerSet:", powerSet)
+
+        print("powerSet [1]   ::", createPowerset([1]))
+        print("powerSet [1,2] ::", createPowerset([1,2]))
+        print("powerSet [1,3] ::", createPowerset([1,2,3]))
         pass
-    
+
     #     # empty or one item lists are always true
     #     self.assertEqual(True, canAdvanceToEnd([]))
     #     self.assertEqual(True, canAdvanceToEnd([1]))
