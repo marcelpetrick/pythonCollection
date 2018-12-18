26bc67875373d4a1f2645c6927a012ef86f720f5
Marcel Petrick <mail@marcelpetrick.it>
2018-12-18 11:34:26 +0100

    day201: maximum weight sum - added task and husk
    Change-Id: I5187a76392bebe4be68df373bfdc075790377e45

---
+++ dailyCodingTask/day201_maxWeightSum.py
@@ -0,0 +1,36 @@
+# This problem was asked by Google.
+#
+# You are given an array of arrays of integers, where each array corresponds to a row in a triangle of numbers.
+# For example, [[1], [2, 3], [1, 5, 1]] represents the triangle:
+#
+#   1
+#  2 3
+# 1 5 1
+#
+# We define a path in the triangle to start at the top and go down one row at a time to an adjacent value,
+# eventually ending with an entry on the bottom row. For example, 1 -> 3 -> 5. The weight of the path is
+# the sum of the entries.
+#
+# Write a program that returns the weight of the maximum weight path.
+
+# ------------------------------------------------------------------------------
+# idea:
+# *
+# ------------------------------------------------------------------------------
+
+import unittest
+
+# ------------------------------------------------------------------------------
+
+# todo: add solution here
+
+# ------------------------------------------------------------------------------
+
+class Testcase(unittest.TestCase):
+    def test_rotateByOne(self):
+        #self.assertEqual(True, [] == rotateByOne([]))
+        pass
+
+# ---- here comes the execution of the unit-tests ----
+if __name__ == '__main__':
+    unittest.main()
