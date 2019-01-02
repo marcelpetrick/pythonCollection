73e38aa1e9cc60f7e4fb900bddbf09a8c8704620
Marcel Petrick <mail@marcelpetrick.it>
2019-01-02 15:13:40 +0100

    day216: working conversion for simple additive cases
    Change-Id: I2384e20efa5b3dbe4b8dccafb426553ecb95f8e8

--- dailyCodingTask/day216_romanNumbers.py
+++ dailyCodingTask/day216_romanNumbers.py
@@ -23,3 +23,49 @@
 import unittest
 
 # ------------------------------------------------------------------------------
+
+conversionTable = {
+    'M': 1000,
+    'D': 500,
+    'C': 100,
+    'L': 50,
+    'X': 10,
+    'V': 5,
+    'I': 1
+}
+
+# ------------------------------------------------------------------------------
+
+def convertRomantToDecimal(input):
+    # first: convert to upp-case
+    romanNumber = str(input).upper()
+    print("romanNumber: ", romanNumber)
+
+    # check if it only consists of the allowed letters
+    # TODO
+
+    # convert the given input
+    decimalValue = 0
+    for digit in romanNumber:
+        print("\t", conversionTable[digit])
+        decimalValue += conversionTable[digit]
+
+    return decimalValue
+
+# ------------------------------------------------------------------------------
+# proper unit-test
+class Testcase(unittest.TestCase):
+    def test_simpleNumbers(self):
+        self.assertEqual(1, convertRomantToDecimal("I"))
+        self.assertEqual(2, convertRomantToDecimal("II"))
+        self.assertEqual(3, convertRomantToDecimal("III"))
+        self.assertEqual(1, convertRomantToDecimal("i"))
+        self.assertEqual(2, convertRomantToDecimal("ii"))
+        self.assertEqual(3, convertRomantToDecimal("iii"))
+
+        self.assertEqual(1666, convertRomantToDecimal("MDCLXVI"))
+
+
+# ---- here comes the execution of the unit-tests ----
+if __name__ == '__main__':
+    unittest.main()
