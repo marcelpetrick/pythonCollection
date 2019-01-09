316df174eb982757f2d86762b7cd50d346dabd33
Marcel Petrick <mail@marcelpetrick.it>
2019-01-09 17:27:33 +0100

    day222: added hint from the msdn-blog
    Change-Id: Ib7ea7e63ae8818e4f7739e103c62a5c4cac993ed

--- dailyCodingTask/day222_shortestStandardizedPath.py
+++ dailyCodingTask/day222_shortestStandardizedPath.py
@@ -6,6 +6,19 @@
 
 # ------------------------------------------------------------------------------
 
+# from: https://blogs.msdn.microsoft.com/jeremykuhne/2016/04/21/path-normalization/
+# Evaluating Relative Components
+#
+# As the path is processed, any components/segments that are comprised of a single or double period are evaluated.
+# For a single period (.) the current segment is removed (as it means current directory).
+# For a double period (..) the current segment and the parent segment are removed (as it means parent directory).
+#
+# Parent directories are only removed if they aren't past the "root" of the path.
+# The root of the path depends on the type of path. It is the drive (C:\) for DOS paths,
+# the server/share for UNCs (\\Server\Share), and the device path prefix for device paths (\\?\ or \\.\).
+
+# ------------------------------------------------------------------------------
+
 import unittest
 import re # for regular expression checking
 
