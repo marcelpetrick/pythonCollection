27c87dc51478ab8ab64b079312a5e6176596c2da
Marcel Petrick <mail@marcelpetrick.it>
2019-01-09 17:24:31 +0100

    day222: added the task for "find the shortest standardized path".
    Change-Id: I5fed26c4a0667240f304f3f66025015bcd50a45a

---
+++ dailyCodingTask/day222_shortestStandardizedPath.py
@@ -0,0 +1,12 @@
+# This problem was asked by Quora.
+#
+# Given an absolute pathname that may have . or .. as part of it, return the shortest standardized path.
+#
+# For example, given "/usr/bin/../bin/./scripts/../", return "/usr/bin/".
+
+# ------------------------------------------------------------------------------
+
+import unittest
+import re # for regular expression checking
+
+# ------------------------------------------------------------------------------
