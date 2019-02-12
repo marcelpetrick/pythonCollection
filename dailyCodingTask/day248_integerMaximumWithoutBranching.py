0eff884072cab7d1585e0aa05f1c2831ce9a5c47
Marcel Petrick <mail@marcelpetrick.it>
2019-02-12 14:41:22 +0100

    day248: working version
    Change-Id: Iafaa980ce81db0bcdcc9459a8ee0123dba2f9264

--- dailyCodingTask/day248_integerMaximumWithoutBranching.py
+++ dailyCodingTask/day248_integerMaximumWithoutBranching.py
@@ -13,7 +13,7 @@ import unittest
 # ------------------------------------------------------------------------------
 
 def maximum(a, b):
-    result = b ^ ((a ^ b) & -(a < b))
+    result = a ^ ((a ^ b) & -(a < b))
 
     return result
 
@@ -23,17 +23,17 @@ class Testcase(unittest.TestCase):
     def test_maximum0(self):
         a, b = 0, 0
         expectedOutput = 0
-        computedOutput = maximum(0,0)
+        computedOutput = maximum(a, b)
         self.assertEqual(expectedOutput, computedOutput)
 
     def test_maximum1(self):
         a, b = 2, 3
         expectedOutput = 3
-        computedOutput = maximum(0,0)
+        computedOutput = maximum(a, b)
         self.assertEqual(expectedOutput, computedOutput)
 
     def test_maximum2(self):
         a, b = 3, 2
         expectedOutput = 3
-        computedOutput = maximum(0,0)
+        computedOutput = maximum(a, b)
         self.assertEqual(expectedOutput, computedOutput)
