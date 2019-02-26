d1d4b3d57178feee8b68eba521d64590745d055d
Marcel Petrick <mail@marcelpetrick.it>
2019-02-26 11:18:53 +0100

    added debugging code
    Change-Id: I8571d8fd58c76f1bfdc86f2696fa80a16fd51a99

--- squashMultipleWhitespace/squashMultipleWhitespace.py
+++ squashMultipleWhitespace/squashMultipleWhitespace.py
@@ -1,10 +1,17 @@
 import sys
 
+# idea:
+# shifting window of two lines over the whole file. if both lines are emtpy, then discard it by not printing. else print "last line".
+
 lastLine = ""
 currentLine = ""
 
 for line in sys.stdin:
+    print("-----------------------------")
     currentLine = line
+    print("lastLine:", lastLine) # todom remove
+    print("currentLine:", currentLine)  # todom remove
+
     if(len(currentLine) != 0 or len(lastLine) != 0):
         sys.stdout.write(lastLine)
 
