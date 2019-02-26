7ede67ec1fa42afec57bb27b5cdbf57d545b3ab2
Marcel Petrick <mail@marcelpetrick.it>
2019-02-26 11:27:18 +0100

    squashMultipleWhitespace: working implementation
    Change-Id: Ida74ec1bb0063de4885512bf9bae830452f46b3d

--- squashMultipleWhitespace/squashMultipleWhitespace.py
+++ squashMultipleWhitespace/squashMultipleWhitespace.py
@@ -7,15 +7,19 @@ lastLine = ""
 currentLine = ""
 
 for line in sys.stdin:
-    print("-----------------------------")
+    #print("-----------------------------")
+    line = line.replace("\n", "").replace("\r", "")
+
     currentLine = line
-    print("lastLine:", lastLine) # todom remove
-    print("currentLine:", currentLine)  # todom remove
+    #print("lastLine:", lastLine) # todom remove
+    #print("currentLine:", currentLine)  # todom remove
 
     if(len(currentLine) != 0 or len(lastLine) != 0):
         sys.stdout.write(lastLine)
+        sys.stdout.write("\n")
 
     lastLine = currentLine
 
 # print the last buffered line
 sys.stdout.write(currentLine)
+sys.stdout.write("\n")
