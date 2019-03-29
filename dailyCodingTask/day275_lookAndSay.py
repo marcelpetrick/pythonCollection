31ce71783d4e4e90c1ef7a5951a7f77952d88333
Marcel Petrick <mail@marcelpetrick.it>
2019-03-29 14:43:11 +0100

    day275: outlined idea; created first helper-function to split the given string into blocks
    Change-Id: Ib7142437d1ae91b4bf6ce0cf98d985394d4ae12d

--- dailyCodingTask/day275_lookAndSay.py
+++ dailyCodingTask/day275_lookAndSay.py
@@ -15,7 +15,10 @@
 
 # ------------------------------------------------------------------------------
 
-# idea: TODO
+# idea:
+# * split the given string into blocks of equal digits
+# * count the length of that block and determine the "type"; then create a block-result like: "n""type"
+# * combine all block results
 
 # ------------------------------------------------------------------------------
 
@@ -24,12 +27,38 @@ import unittest
 # ------------------------------------------------------------------------------
 
 def lookAndSay(n):
-    # todo return the nth term of the sequence
+    # todo return the nth term of the "look and say"-sequence
+
+    if n <= 0:
+        raise ValueError("Pascal's Triangle only defined for integer values bigger 0")
 
     if(n == 1):
         return 1
 
+
     pass
+
+# ------------------------------------------------------------------------------
+
+def createBlockList(input):
+    # take String; split into blocks of similar digits
+
+    if len(input) == 0:
+        raise Exception("input too short")
+
+    blockList = []
+    separator = input[0]
+    currentBlock = ""
+    for digit in input:
+        if digit == separator:
+            currentBlock += digit
+        else:
+            separator = digit
+            blockList.append(currentBlock)
+            currentBlock = digit
+    blockList.append(currentBlock)
+
+    return blockList
 
 # ------------------------------------------------------------------------------
 # proper unit-test
@@ -39,3 +68,42 @@ class Testcase(unittest.TestCase):
         expectedOutput = 1
         computedOutput = lookAndSay(n)
         self.assertEqual(expectedOutput, computedOutput)
+
+    # TODO add a test which checks for the thrown exception
+    def test_exceptionThrowing(self):
+        self.assertRaises(ValueError, lookAndSay, -1337)
+
+    # tests for createBlockList
+    def test_createBlockListException(self):
+        self.assertRaises(Exception, createBlockList, "")
+
+    def test_createBlockList0(self):
+        input = "2"
+        expectedOutput = ["2"]
+        computedOutput = createBlockList(input)
+        self.assertEqual(expectedOutput, computedOutput)
+
+    def test_createBlockList1(self):
+        input = "222"
+        expectedOutput = ["222"]
+        computedOutput = createBlockList(input)
+        self.assertEqual(expectedOutput, computedOutput)
+
+    def test_createBlockList2(self):
+        input = "1337"
+        expectedOutput = ["1", "33", "7"]
+        computedOutput = createBlockList(input)
+        self.assertEqual(expectedOutput, computedOutput)
+
+    def test_createBlockList3(self):
+        input = "1223334112351"
+        expectedOutput = ["1", "22", "333", "4", "11", "2", "3", "5", "1"]
+        computedOutput = createBlockList(input)
+        self.assertEqual(expectedOutput, computedOutput)
+
+# ---- here comes the execution of the unit-tests ----
+if __name__ == '__main__':
+    unittest.main()
+
+
+createBlockList("1223334112351")
