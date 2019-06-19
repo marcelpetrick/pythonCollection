7735642d7fdc745c10e348adb2a3ae09f0f45d18
Marcel Petrick <mail@marcelpetrick.it>
2019-06-19 16:50:20 +0200

    day362: working implementation for the number-check
    Change-Id: Ie139236539662f102987331569728cc2661dd0ce

--- dailyCodingTask/day362_strobogrammaticNumber.py
+++ dailyCodingTask/day362_strobogrammaticNumber.py
@@ -7,11 +7,14 @@
 
 # ------------------------------------------------------------------------------
 
-# idea: TODO
-# 1 is rotat-able (is this a word?) to 1
-# 2 to 5?
+# idea: if string is of length 0 or 1: is strobo; else: compare the first and last character for rotation-awareness;
+# then apply the function to the inner string
+#
+# 1 is rotatable (is this a word?) to 1
+# 2 to 5? (i would say yes, but this depends on the type of glyphs which are used; is a detail ..)
 # 6 to 9
 # 8 to itself
+# 3, 4, 7 are fitting to nothing ... so if first/last is one of those, then discard
 
 # ------------------------------------------------------------------------------
 
@@ -19,13 +22,39 @@ import unittest
 
 # ------------------------------------------------------------------------------
 
+# added also the switched case to this list
+fittingPairs = [(1, 1),
+                (2, 5),
+                (5, 2), # remove if 5 =/= 2
+                (6, 9),
+                (8, 8), # remove if 5 =/= 2
+                (9, 6),
+                ]
+
 def isStroboGrammmatic(number):
     # todo description of the method
+    # number should be an integer
 
-    if(number == 1 or number == 8 or number == 69):
+    strNumber = str(number)
+
+    if len(strNumber)  < 2:
+        # strings of length 0 or 1 are always strobo
         return True
 
-    return False
+    # get the head and tail and convert them back to int
+    first = int(strNumber[:1])
+    last = int(strNumber[-1:])
+    #print(first, last) # todo remove
+    middle = strNumber[1:-1]
+    #print("middle:", middle) # todo remove
+
+    headAndTailAreStrobo = False
+    for a,b in fittingPairs:
+        if a == first and b == last:
+            headAndTailAreStrobo = True
+
+
+    return headAndTailAreStrobo
 
 # ------------------------------------------------------------------------------
 # proper unit-test
@@ -36,11 +65,24 @@ class Testcase(unittest.TestCase):
         computedOutput = isStroboGrammmatic(number)
         self.assertEqual(expectedOutput, computedOutput)
 
+    def test_givenExample1(self):
+        number = 123
+        expectedOutput = False
+        computedOutput = isStroboGrammmatic(number)
+        self.assertEqual(expectedOutput, computedOutput)
+
+    def test_givenExample2(self):
+        number = 4
+        expectedOutput = True
+        computedOutput = isStroboGrammmatic(number)
+        self.assertEqual(expectedOutput, computedOutput)
+
     def test_expectFail(self):
         self.assertFalse(1 < 0, "hell has frozen over")
 
+
 # ---- here comes the execution of the unit-tests ----
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
 
 # ------------------------------------------------------------------------------
