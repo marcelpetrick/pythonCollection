3b6eda96bb12422398e7e5afe83e396c6acf27a1
Marcel Petrick <mail@marcelpetrick.it>
2019-07-03 16:39:04 +0200

    problem668: working impl.  - but check for range missing

--- ProjectEuler/problem668_squareRootSmooth.py
+++ ProjectEuler/problem668_squareRootSmooth.py
@@ -8,11 +8,38 @@
 
 # ------------------------------------------------------------------------------
 
-# idea: TODO
+# examples: https://oeis.org/A048098
+
+# ------------------------------------------------------------------------------
+
+# idea: simplest version: loop for the whole range, check for each if SRS. if yes, then add up
+#
 
 # ------------------------------------------------------------------------------
 
 import unittest
+
+import math # for square root
+
+# ------------------------------------------------------------------------------
+
+def isSquareRootSmooth(number):
+    ''' check if get sadfasdf is smaller than the square root'''
+
+    squareRoot = math.sqrt(number)
+    biggestPrime = getBiggestPrimeFactor(number) # add some dictionary LUT for preformance
+
+    return biggestPrime < squareRoot
+
+# ------------------------------------------------------------------------------
+
+def getBiggestPrimeFactor(number):
+
+    # check invalid values
+    if(number < 1):
+        raise Exception("invalid input")
+
+    return max(getPrimeFactors(number))
 
 # ------------------------------------------------------------------------------
 
@@ -36,23 +63,8 @@ def getPrimeFactors(number):
 
 # ------------------------------------------------------------------------------
 
-def isSquareRootSmooth(n):
-    # todo description of the method
-
-    if(n == 1):
-        return True
-
-    pass
-
-# ------------------------------------------------------------------------------
-
 # proper unit-test
 class Testcase(unittest.TestCase):
-    def test_givenExample0(self):
-        n = 1
-        expectedOutput = True
-        computedOutput = isSquareRootSmooth(n)
-        self.assertEqual(expectedOutput, computedOutput)
 
     def test_getPrimeFactors(self):
         self.assertEqual([], getPrimeFactors(-1))
@@ -61,6 +73,21 @@ class Testcase(unittest.TestCase):
         self.assertEqual([2, 2], getPrimeFactors(4))
         self.assertEqual([2, 2, 2, 2, 2, 2, 2], getPrimeFactors(128))
         self.assertEqual([2, 3, 5, 7], getPrimeFactors(210))
+
+    def test_getBiggestPrimeFactor(self):
+        self.assertEqual(2, getBiggestPrimeFactor(2))
+        self.assertEqual(5, getBiggestPrimeFactor(5))
+        self.assertEqual(7, getBiggestPrimeFactor(210))
+
+    def test_isSquareRootSmooth(self):
+        self.assertEqual(True, isSquareRootSmooth(40));
+        self.assertEqual(False, isSquareRootSmooth(41));
+        self.assertEqual(True, isSquareRootSmooth(45));
+        self.assertEqual(True, isSquareRootSmooth(189));
+        self.assertEqual(False, isSquareRootSmooth(190));
+        self.assertEqual(False, isSquareRootSmooth(191));
+        self.assertEqual(True, isSquareRootSmooth(192));
+        self.assertEqual(True, isSquareRootSmooth(195));
 
 # todo add more tests
 
