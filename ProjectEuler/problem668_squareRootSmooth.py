8ffeb92ac8630612b0a840bd30db3e57aab8f198
Marcel Petrick <mail@marcelpetrick.it>
2019-07-04 16:12:43 +0200

    problem668: added becnhmarking

--- ProjectEuler/problem668_squareRootSmooth.py
+++ ProjectEuler/problem668_squareRootSmooth.py
@@ -2,7 +2,7 @@
 # Problem 668
 #
 # A positive integer is called square root smooth if all of its prime factors are strictly less than its square root.
-# Including the number 1, there are 29 square root smooth numbers not exceeding 100.
+# ah
 #
 # How many square root smooth numbers are there not exceeding 10000000000?
 
@@ -21,12 +21,14 @@ import unittest
 
 import math # for square root
 
+import time
+
 # ------------------------------------------------------------------------------
 
 def getNumberOfSRSBelow(number):
     amount = 0
     #resultList = []
-    for currentN in range(1, number):
+    for currentN in range(1, number + 1):
         isSRS = isSquareRootSmooth(currentN)
         #print(f"number {currentN} is", ("" if isSRS else "not"), "square root smooth")
         if(isSRS):
@@ -37,7 +39,7 @@ def getNumberOfSRSBelow(number):
     #print(f"below {number} are {amount} numbers square-root-smooth")
     #print("result list: ", resultList)
 
-    return amount
+    return amount + 1 # plus one for the number "1" itself, because the task-description is including it
 
 # ------------------------------------------------------------------------------
 
@@ -55,8 +57,8 @@ def isSquareRootSmooth(number):
           # "square root smooth"
           # )
 
-    if result:
-        print(number)
+#    if result:
+#        print(number)
 
     return result
 
@@ -125,7 +127,7 @@ class Testcase(unittest.TestCase):
 
     def test_getNumberOfSRSBelow(self):
         '''
-        Although I am not fully convinced this is the real
+        Although I am not fully convinced this is the real, because of the diverting comparators
         1, 4, 8, 9, 12, 16, 18, 24, 25, 27, 30, 32, 36, 40, 45, 48, 49, 50, 54, 56,
         60, 63, 64, 70, 72, 75, 80, 81, 84, 90,
         96, 98, 100, 105, 108, 112, 120, 121, 125, 126, 128, 132, 135, 140, 144, 147, 150,
@@ -134,7 +136,7 @@ class Testcase(unittest.TestCase):
 
         amount = getNumberOfSRSBelow(100)
         #print("below 100:", amount)
-        self.assertEqual(27, amount) # TODO should be 29!
+        self.assertEqual(29, amount) # TODO should be 29!
 
 # ---- here comes the execution of the unit-tests ----
 if __name__ == '__main__':
@@ -143,5 +145,18 @@ if __name__ == '__main__':
 # ------------------------------------------------------------------------------
 
 # real task
-answer = getNumberOfSRSBelow(10000000000)
-print("below 10000000000:", answer)
+# answer = getNumberOfSRSBelow(10000000000)
+# print("below 10000000000:", answer)
+
+
+#print(f"is SRS? 10.000.000.000 - {isSquareRootSmooth(10000000000)}")
+
+# benchmark for different ranges the computation a bit. Rises quite fast: first 0s, 0s, 0s, 1s, 122s .. without optimization
+for digits in range(1, 4):
+    startTime = time.time()
+    #print("digits:",digits)
+    number = 10 ** digits
+    #print("number:", number)
+
+    print("for range up to ", number, "the amount of SRS is:", getNumberOfSRSBelow(number))
+    print(f"\t computation time: {time.time() - startTime} s" )
