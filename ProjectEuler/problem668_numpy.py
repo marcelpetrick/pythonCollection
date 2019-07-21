8796912a1e7c3e08ad4fb367242544ca0f043c61
Marcel Petrick <mail@marcelpetrick.it>
2019-07-21 16:39:00 +0200

    problem668: timing the function

--- ProjectEuler/problem668_numpy.py
+++ ProjectEuler/problem668_numpy.py
@@ -85,14 +85,16 @@ def getNumberOfSRSBelow(limit):
 
 def sieveEras(limit):
     isPrimeArray = np.full(limit, True, dtype = bool)
-    print("before:", isPrimeArray)
+    #print("before:", isPrimeArray)
 
     # zero and one are by definition no prime
     isPrimeArray[0] = False
     isPrimeArray[1] = False
 
     index = 0
-    for index in range(int(limit ** 0.5)):
+    #print("going up to:", range(int(limit ** 0.5)))
+    for index in range(int(limit ** 0.5) + 1):
+        #print("handling:", index)
         if isPrimeArray[index] == False:
             continue
         else:
@@ -100,6 +102,8 @@ def sieveEras(limit):
             while index * multiple < limit:
                 isPrimeArray[index * multiple] = False
                 multiple += 1
+
+        #print("\tnow:", isPrimeArray)
 
     # map to numbers
     resultList = []
@@ -114,15 +118,37 @@ def sieveEras(limit):
 # proper unit-test
 class Testcase(unittest.TestCase):
 
-    def test_sieveEras(self):
+    def test_sieveEras10(self):
         limit = 10
         result = sieveEras(limit)
-        print("computed primes until", limit, " --> ", result)
+        #print("computed primes until", limit, " --> ", result)
 
-        expectedPrimes = [2,3,5,7] #,11,13,17]
+        expectedPrimes = [2,3,5,7]
+        self.assertEqual(expectedPrimes, result)
+
+    def test_sieveEras100(self):
+        limit = 100
+        result = sieveEras(limit)
+        #print("computed primes until", limit, " --> ", result)
+
+        expectedPrimes = 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+        self.assertEqual(expectedPrimes, result)
+
+    def test_sieveEras1000(self):
+        limit = 1000
+        result = sieveEras(limit)
+        #print("computed primes until", limit, " --> ", result)
+
+        expectedPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
+        self.assertEqual(expectedPrimes, result)
 
 # ---- here comes the execution of the unit-tests ----
 if __name__ == '__main__':
     unittest.main()
 
 # ------------------------------------------------------------------------------
+import time
+print("###############")
+for i in range(2, 8 + 1):
+    startTime = time.time()
+    print("how many primes in 10 ^", i, "?", len(sieveEras(10 ** i)), "in", time.time() - startTime, "s")
