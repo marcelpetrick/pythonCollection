4fbc4606a54d31cc652371eeb377297b38b216fa
Marcel Petrick <mail@marcelpetrick.it>
2019-08-20 17:06:48 +0200

    financeCalender: minor testing

--- financeCalender/financeCalender.py
+++ financeCalender/financeCalender.py
@@ -17,3 +17,18 @@ print(cal)
 # note sure if this is helpful
 
 # needed is some iterate-able calender, which has a given start-time, then strides over the days
+
+import time
+
+ticks = time.localtime()
+# --> ticks: time.struct_time(tm_year=2019, tm_mon=8, tm_mday=20, tm_hour=17, tm_min=1, tm_sec=32, tm_wday=1, tm_yday=232, tm_isdst=1)
+
+print("ticks:", ticks)
+
+# https://o7planning.org/de/11443/anleitung-python-date-time
+fakedTime = (2019, 8, 20, 0, 0, 0, 0, 0, 0)
+ticks = time.mktime(fakedTime)
+print("fakedTime:", fakedTime)
+print("ticks:", ticks)
+
+# question: how to jump by "one day"?
