6a6da91710f913ad2369435e92e1846aa017a5d0
Marcel Petrick <mail@marcelpetrick.it>
2019-08-22 13:23:22 +0200

    financeCalender: improved output; made test-run with evaluation next todo: adapt the "trigger"

--- financeCalender/financeCalender.py
+++ financeCalender/financeCalender.py
@@ -1,35 +1,35 @@
-# Python code to demonstrate the working of
-# calendar() function to print calendar
-
-# importing calendar module
-# for calendar operations
-import calendar
-
-# using calender to print calendar of year
-# prints calendar of 2018
-print("The calender of year 2018 is : ")
-print(calendar.calendar(2018, 2, 1, 6))
-cal = calendar.calendar(2109, 1, 1, 1)
-print(cal)
-
-
-# taken from https://www.geeksforgeeks.org/python-calendar-module/
-# note sure if this is helpful
-
-# needed is some iterate-able calender, which has a given start-time, then strides over the days
-
-import time
-
-ticks = time.localtime()
-# --> ticks: time.struct_time(tm_year=2019, tm_mon=8, tm_mday=20, tm_hour=17, tm_min=1, tm_sec=32, tm_wday=1, tm_yday=232, tm_isdst=1)
-
-print("ticks:", ticks)
-
-# https://o7planning.org/de/11443/anleitung-python-date-time
-fakedTime = (2019, 8, 20, 0, 0, 0, 0, 0, 0)
-ticks = time.mktime(fakedTime)
-print("fakedTime:", fakedTime)
-print("ticks:", ticks)
+# # Python code to demonstrate the working of
+# # calendar() function to print calendar
+#
+# # importing calendar module
+# # for calendar operations
+# import calendar
+#
+# # using calender to print calendar of year
+# # prints calendar of 2018
+# print("The calender of year 2018 is : ")
+# print(calendar.calendar(2018, 2, 1, 6))
+# cal = calendar.calendar(2109, 1, 1, 1)
+# print(cal)
+#
+#
+# # taken from https://www.geeksforgeeks.org/python-calendar-module/
+# # note sure if this is helpful
+#
+# # needed is some iterate-able calender, which has a given start-time, then strides over the days
+#
+# import time
+#
+# ticks = time.localtime()
+# # --> ticks: time.struct_time(tm_year=2019, tm_mon=8, tm_mday=20, tm_hour=17, tm_min=1, tm_sec=32, tm_wday=1, tm_yday=232, tm_isdst=1)
+#
+# print("ticks:", ticks)
+#
+# # https://o7planning.org/de/11443/anleitung-python-date-time
+# fakedTime = (2019, 8, 20, 0, 0, 0, 0, 0, 0)
+# ticks = time.mktime(fakedTime)
+# print("fakedTime:", fakedTime)
+# print("ticks:", ticks)
 
 # question: how to jump by "one day"?
 # http://www.pressthered.com/adding_dates_and_times_in_python/
@@ -69,27 +69,35 @@ class TransactionItem:
 
         return False
 
+    # really needed, because all access is public?
+    def getAmount(self):
+        return self.amount
 
+# example transactions
 entry0 = TransactionItem("current state", "once", 100, "22.08.2019")
 entry1 = TransactionItem("consumed food", "daily", -5, "21.08.2019") # starting from today; every day 5 kuan loss
 entry2 = TransactionItem("salary", "monthly", +666, "01.09.2019") # question: how to represent the monthly stuff? just use the day from date?
 transactions = [entry0, entry1, entry2]
 
-# check
-print("---current state----")
-#for entry in transactions:
-#    print("entry:", entry)
-
 # print with enumeration
+print("--- transactions ----")
 for entry in range(len(transactions)):
     print(entry, transactions[entry])
 
-#------
+#-------- test run of the "program" --------
 startDate = datetime.fromisoformat("2019-08-21")
 currentDateTime = startDate
 print("starting with:", currentDateTime)
 
+currentCash = 2000
+
 # "walks" over the next ten days :)
 for i in range(10):
-    print(currentDateTime)
+    # check for all transactions if they trigger; and if, then evaluate
+    for item in transactions:
+        if item.willTriggerToday(currentDateTime):
+            currentCash += item.getAmount()
+
+    # print the current state
+    print(i,":", currentDateTime, "cash=", currentCash)
     currentDateTime += timedelta(days=1)
