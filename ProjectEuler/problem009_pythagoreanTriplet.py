9c0f8328e910b9cd85ea479105f595e0200dc942
Marcel Petrick <mail@marcelpetrick.it>
2019-09-18 17:37:37 +0200

    euler009: added some generation-idea

--- ProjectEuler/problem009_pythagoreanTriplet.py
+++ ProjectEuler/problem009_pythagoreanTriplet.py
@@ -9,6 +9,13 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
+# idea:
+# how to generate them efficently?
+# "Euclid's formula[3] is a fundamental formula for generating Pythagorean triples given an arbitrary pair of integers m and n with m > n > 0. The formula states that the integers
+#
+#     a = m 2 − n 2 ,   b = 2 m n ,   c = m 2 + n 2 {\displaystyle a=m^{2}-n^{2},\ \,b=2mn,\ \,c=m^{2}+n^{2}} a=m^{2}-n^{2},\ \,b=2mn,\ \,c=m^{2}+n^{2}
+#
+# form a Pythagorean triple. "
 
 def pythagoreanTriplet():
     return 1337
