4889decbc93b804ad4fca9538f874a9401eadd0d
Marcel Petrick <mail@marcelpetrick.it>
2019-09-19 17:46:39 +0200

    euler009: corrected structure

--- ProjectEuler/problem009_pythagoreanTriplet.py
+++ ProjectEuler/problem009_pythagoreanTriplet.py
@@ -3,19 +3,28 @@
 # Problem 9
 # A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 #
-# a2 + b2 = c2
-# For example, 32 + 42 = 9 + 16 = 25 = 52.
+# a^2 + b^2 = c^2
+# For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 #
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
+# ------------------------------------------------------------------------------
+
 # idea:
-# how to generate them efficently?
-# "Euclid's formula[3] is a fundamental formula for generating Pythagorean triples given an arbitrary pair of integers m and n with m > n > 0. The formula states that the integers
+# how to generate them efficiently?
 #
-#     a = m 2 − n 2 ,   b = 2 m n ,   c = m 2 + n 2 {\displaystyle a=m^{2}-n^{2},\ \,b=2mn,\ \,c=m^{2}+n^{2}} a=m^{2}-n^{2},\ \,b=2mn,\ \,c=m^{2}+n^{2}
-#
-# form a Pythagorean triple. "
+# from en.wikipedia.org: "Euclid's formula[3] is a fundamental formula for generating Pythagorean triples given an
+# arbitrary pair of integers m and n with m > n > 0. The formula states that the integers#
+#     a = m 2 − n 2 ,   b = 2 m n ,   c = m 2 + n 2
+#     {\displaystyle a=m^{2}-n^{2},\ \,b=2mn,\ \,c=m^{2}+n^{2}} a=m^{2}-n^{2},\ \,b=2mn,\ \,c=m^{2}+n^{2}
+# form a Pythagorean triple."
+
+# ------------------------------------------------------------------------------
+# implementation
+# ------------------------------------------------------------------------------
+import unittest
+
 
 def pythagoreanTriplet():
     return 1337
