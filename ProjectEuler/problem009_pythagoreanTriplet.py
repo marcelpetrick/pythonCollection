522fe1af10673feb191813a38e3d9a9339f6286c
Marcel Petrick <mail@marcelpetrick.it>
2019-09-20 15:44:47 +0200

    euler009: naive implementation

--- ProjectEuler/problem009_pythagoreanTriplet.py
+++ ProjectEuler/problem009_pythagoreanTriplet.py
@@ -19,14 +19,59 @@
 #     a = m 2 − n 2 ,   b = 2 m n ,   c = m 2 + n 2
 #     {\displaystyle a=m^{2}-n^{2},\ \,b=2mn,\ \,c=m^{2}+n^{2}} a=m^{2}-n^{2},\ \,b=2mn,\ \,c=m^{2}+n^{2}
 # form a Pythagorean triple."
-
+#
+# assumption: since c with highest value of 1000 (itself) is the upper limit, just search for values up to 1k.
+# c is the sum of m^2 and n^2, where m is bigger n. So it could be topped by m <= 1000 too.
+#
+# plan: generate two loops: m from 1000 to 0; n from m-1 to 0: then generate a, b and c. check if pyth; if yes, then check if sum is 1k.
 # ------------------------------------------------------------------------------
 # implementation
 # ------------------------------------------------------------------------------
+
 import unittest
 
+def isPythagoreanTriplet(a, b, c):
+    leftSide = a * a + b * b
+    rightSide = c * c
+    isPythagoreanTriplet = (leftSide == rightSide)
+    return isPythagoreanTriplet
 
-def pythagoreanTriplet():
-    return 1337
+# --------------------
 
-print(pythagoreanTriplet())
+def generateABC(m, n):
+    mSquare = m * m
+    nSquare = n * n
+
+    a = mSquare - nSquare
+    b = 2 * m * n
+    c = mSquare + nSquare
+
+    return a,b,c
+
+# --------------------
+
+def isSum1000(a,b,c):
+    return (a + b + c == 1000)
+
+# --------------------
+
+def __main__():
+    upperLimit = 100
+
+    for m in range(upperLimit, -1, -1):
+        for n in range(m, -1, -1):
+            a,b,c = generateABC(m, n)
+
+            foundProperTriplet = isSum1000(a,b,c)
+
+            print("m, n:", m, n, "a,b,c:", a,b,c, "is Hit:", foundProperTriplet, a+b+c)
+
+            # hard exit, IDC
+            if foundProperTriplet:
+                print("product for the solution is:", a*b*c)
+                exit(0)
+
+# --------------------
+
+# run it
+__main__()
