5865156421399791906aaa165f7758aab1afce21
Marcel Petrick <mail@marcelpetrick.it>
2019-10-08 00:55:51 +0200

    euler067: took over the code from 018

--- ProjectEuler/problem067_maximumPathSumII.py
+++ ProjectEuler/problem067_maximumPathSumII.py
@@ -28,3 +28,76 @@
 # ------------------------------------------------------------------------------
 # implementation
 # ------------------------------------------------------------------------------
+
+# reusing the code would work like this
+# from ProjectEuler import problem018_maximumPathSum as p018
+# p018.processTriangle()
+# but there are some flaws, because no proper separation of functionality .... so let us just copy and modify the stuff
+
+
+def prepareInput():
+    inputString = (
+         "75"
+        ,"95 64"
+        ,"17 47 82"
+        ,"18 35 87 10"
+        ,"20 04 82 47 65"
+        ,"19 01 23 75 03 34"
+        ,"88 02 77 73 07 63 67"
+        ,"99 65 04 28 06 16 70 92"
+        ,"41 41 26 56 83 40 80 70 33"
+        ,"41 48 72 33 47 32 37 16 94 29"
+        ,"53 71 44 65 25 43 91 52 97 51 14"
+        ,"70 11 33 28 77 73 17 78 39 68 17 57"
+        ,"91 71 52 38 17 14 91 43 58 50 27 29 48"
+        ,"63 66 04 68 89 53 67 30 73 16 69 87 40 31"
+        ,"04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"
+    )
+
+    # convert all lines to integer-lists for the list of the triangle
+    # the list(map(int, ..)) converts the split result to integers
+    listOfNumbers = [list(map(int, string.split(" "))) for string in inputString]
+
+    return listOfNumbers
+
+# ------------------------------------------------------------------------------
+
+#print(prepareInput())
+
+# algo: (loop from bottom-1 to top):
+# 0. take line, go from left to right: put for each pair the maximum as new elem (not in place!)
+# 1. add the line above to it
+
+def processTriangle():
+    # prearation: read input and reverse
+    original = prepareInput()
+    # reverse IN PLACE!
+    original.reverse()
+    print(original) # todo remove
+
+    # prepare a temporary line
+    initialLengthFirstRow = len(original[0])
+    print("initialLengthFirstRow:", initialLengthFirstRow)
+
+    lastLine = [0] * (initialLengthFirstRow + 1)
+    print(lastLine)
+
+    # sum up
+    for index in range(len(original)):
+        # create a new selected list out of the temporary
+        pickedMaxList = []
+        for listIndex in range(len(lastLine) - 1):
+            a = lastLine[listIndex + 0]
+            b = lastLine[listIndex + 1]
+            pickedMaxList.append(max(a, b))
+        print(pickedMaxList)
+
+        # sum with temporary line
+        lastLine = [x+y for x,y in zip(pickedMaxList, original[index])]
+        print(lastLine)
+
+    # first element should be now the result
+    print("final state:", lastLine)
+
+# -- test call --
+processTriangle()
