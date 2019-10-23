969aaf8e71cc7d979a227dce9f11fd6e2fdebc77
Marcel Petrick <mail@marcelpetrick.it>
2019-10-23 23:12:15 +0200

    euler022: continued implementation

--- ProjectEuler/problem022_nameScore.py
+++ ProjectEuler/problem022_nameScore.py
@@ -1,9 +1,12 @@
 # Names scores
 #
 # Problem 22
-# Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
+# Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand
+# first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name,
+# multiply this value by its alphabetical position in the list to obtain a name score.
 #
-# For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+# For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53,
+# is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
 #
 # What is the total of all the name scores in the file?
 
@@ -16,13 +19,13 @@
 # ------------------------------------------------------------------------------
 #import time
 #import unitttest
-
+# ------------------------------------------------------------------------------
 def prepareInput():
 
     # open
     file = open('problem022_names.txt', 'r')
 
-    # result container
+    # prepare result container
     listOfCleanedNames = list()
 
     # read content
@@ -30,11 +33,28 @@ def prepareInput():
         splitLine = line.split(",")
         for item in splitLine:
             cleanItem = item.replace("\"", "")
-            print("cleanItem:", cleanItem)
+            #print("cleanItem:", cleanItem)
             listOfCleanedNames.append(cleanItem)
 
     return listOfCleanedNames
+# ------------------------------------------------------------------------------
+def calcValoeOfName(name):
+
+    return 1
+
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 
 #--- test call ---
 
-print(prepareInput())
+inputList = prepareInput()
+print("inputList:", inputList)
+inputList.sort();
+print("inputList:", inputList)
+
+position = 0
+for name in inputList:
+    finalValue = 2 * 3 # todo
+    print(name, calcValoeOfName(name), position, finalValue)
+    position -=-1
