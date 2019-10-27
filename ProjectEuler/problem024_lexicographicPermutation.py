b21bd9a7ac7cb181e0f9f4794a70fd1cfaee8e6c
Marcel Petrick <mail@marcelpetrick.it>
2019-10-27 23:46:58 +0100

    euler024: naive implementation

--- ProjectEuler/problem024_lexicographicPermutation.py
+++ ProjectEuler/problem024_lexicographicPermutation.py
@@ -10,3 +10,21 @@
 # What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
 # ----------------
+
+# idea: brute force; just count upwards, check if the number would be a valid permutation ..
+
+# --------------
+
+# init
+comparisonList = []
+for i in range(0,10):
+    comparisonList.append(str(i))
+
+def isValidPermutation(number):
+    asListOfChars = [x for x in str(number)]
+    asListOfChars.sort()
+    print("as char:", asListOfChars)
+    return asListOfChars == comparisonList
+
+print(comparisonList)
+print("1234567890:", isValidPermutation(1234567890))
