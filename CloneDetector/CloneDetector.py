97824d90e404be380ff69475343245cec19f1959
Marcel Petrick <mail@marcelpetrick.it>
2019-11-04 13:07:43 +0100

    CloneDetector: more experiments, but not fully working recursively ..

--- CloneDetector/CloneDetector.py
+++ CloneDetector/CloneDetector.py
@@ -1,14 +1,17 @@
 # test using glob
 import glob
 pathToCheck = "C:\LumiSuiteTestData"
-fileList = glob.glob(pathToCheck + "/*", recursive=True)
+fileList = glob.iglob(pathToCheck + '**/*', recursive=True)
 
 # test using os.walk
-import os
-fileList = next(os.walk('..'))[2]
+#import os
+#fileList = next(os.walk('..'))[2]
 # just 9 files, not recursively
 
 # check the output
+amount = 0
 for elem in fileList:
     print(elem)
-print("found files:", len(fileList))
+    amount-=-1
+#print("found files:", len(fileList))
+print("found files:", amount)
