4be55943dd18323659100dcb74770e0f34e51114
Marcel Petrick <mail@marcelpetrick.it>
2019-11-05 09:32:43 +0100

    CloneDetector: cleaned code

--- CloneDetector/CloneDetector.py
+++ CloneDetector/CloneDetector.py
@@ -13,12 +13,12 @@ def md5(filename):
 
 # -------------------------------------------
 
-def sizeof_fmt(num, suffix='B'):
+def humanReadableFormat(num, suffix='B'):
     # human readable version of the file size:
     # taken from: https://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
     for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
         if abs(num) < 1024.0:
-            return "%3.1f%s%s" % (num, unit, suffix)
+            return "%3.1 f%s%s" % (num, unit, suffix)
         num /= 1024.0
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
@@ -30,12 +30,12 @@ pathToCheck = "C:\LumiSuiteTestData"
 
 # TODO make this a real function with time measurement!
 fileList = []
-for filename in Path(pathToCheck).rglob('*.*'): # TODO run this several times to catch different file types; concatenate the lists
+for filename in Path(pathToCheck).rglob('*.*'):
     fileList.append(filename)
 
 import os
 import time
-from collections import defaultdict, deque
+from collections import defaultdict
 hashDict = defaultdict(list)
 # check the output
 for elem in fileList:
@@ -47,13 +47,9 @@ for elem in fileList:
     currentTime = time.time()
     try:
         md5sum = md5(elem)
-        print(elem, ":", sizeof_fmt(os.path.getsize(elem)), ":", md5sum, " - ", time.time() - currentTime, "s")
+        print(elem, ":", humanReadableFormat(os.path.getsize(elem)), ":", md5sum, " - ", time.time() - currentTime, "s")
         # insert into final structure
-        #if md5sum in hashDict:
         hashDict[md5sum].append(str(elem.absolute()))
-        #else:
-#            hashDict[md5sum] = [str(elem.absolute())]
-        #print("current dict:", hashDict)
     except PermissionError:
         print("permission-error with:", str(elem.absolute()))
 
@@ -61,7 +57,7 @@ for elem in fileList:
 print("###################################################")
 print("amount of found files:", len(fileList))
 print("amount of entries in the duplicate dictionary:", len(hashDict))
-# print the ducplicates
+# print the duplicates
 for key in hashDict:
     if len(hashDict[key]) > 1:
-        print("duplo:", len(hashDict[key]), key, sizeof_fmt(os.path.getsize(hashDict[key][0])), hashDict[key])
+        print("duplo:", len(hashDict[key]), key, humanReadableFormat(os.path.getsize(hashDict[key][0])), hashDict[key])
