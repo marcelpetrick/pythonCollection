e599d70cd5b587024c4671a4450e24545139429a
Marcel Petrick <mail@marcelpetrick.it>
2019-11-09 22:49:25 +0100

    euler011: solved; missing direction was the culprit!
    Got Level 1 award! :)

--- ProjectEuler/problem011_largestProductInAGrid.py
+++ ProjectEuler/problem011_largestProductInAGrid.py
@@ -59,7 +59,7 @@ def computeMaximum4Product(matrix):
     biggest = -1
     for x in range(0, sideLength):
         for y in range(0, sideLength):
-            print("handling now:", x, y)
+            #print("handling now:", x, y)
             # --- check to the right ---
             if x + numFactors < sideLength:  # prevent out of bounds!
                 product = 1
@@ -93,6 +93,17 @@ def computeMaximum4Product(matrix):
                     biggest = product
                     print("bottom: product starting from:[", x, ",", y, "] = ", product)
 
+            # --- check to the bottom left! (this was the forgotten one) ---
+            # thanks for the hint to: https://www.mathblog.dk/greatest-product-in-20x20-grid/
+            if x >= numFactors and y + numFactors < sideLength:  # prevent out of bounds!
+                product = 1
+                for i in range(0, numFactors):
+                    product *= matrix[x - i][y + i]
+
+                if product > biggest:
+                    biggest = product
+                    print("bottom left: product starting from:[", x, ",", y, "] = ", product)
+
     return biggest
 
 # ------------------------------------------------------------------------------
@@ -103,11 +114,30 @@ print("matrix:", matrix)
 print("biggest:", computeMaximum4Product(matrix))
 
 # ------------------------------------------------------------------------------
-# ..
-# handling now: 19 18
-# handling now: 19 19
-# biggest: 51267216
+# matrix: [[8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8], [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0], [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65], [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91], [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80], [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50], [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70], [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21], [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72], [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95], [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92], [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57], [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58], [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40], [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66], [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69], [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36], [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16], [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54], [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]]
+# right: product starting from:[ 0 , 0 ] =  1651104
+# right: product starting from:[ 0 , 2 ] =  6414210
+# right: product starting from:[ 0 , 3 ] =  6514520
+# diagonal bottom: product starting from:[ 0 , 7 ] =  11587200
+# diagonal bottom: product starting from:[ 1 , 0 ] =  16194745
+# right: product starting from:[ 1 , 5 ] =  25723980
+# diagonal bottom: product starting from:[ 2 , 1 ] =  32719995
+# bottom left: product starting from:[ 4 , 9 ] =  41076896
+# right: product starting from:[ 6 , 15 ] =  51267216
+# bottom left: product starting from:[ 15 , 3 ] =  70600674
+# biggest: 70600674
+#
+# Process finished with exit code 0
 
-# Sorry, but the answer you gave appears to be incorrect.
+# ------------------------------------------------------------------------------
 
-# ??? confused
+# Congratulations, the answer you gave to problem 11 is correct.
+#
+# You are the 218223rd person to have solved this problem.
+#
+# Nice work, marcelpetrick, you've just advanced to Level 1 .
+# 111783 members (11.67%) have made it this far.
+#
+# You have earned 1 new award:
+#
+# The Journey Begins: Progress to Level 1 by solving twenty-five problems
