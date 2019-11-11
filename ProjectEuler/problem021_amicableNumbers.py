dc655b3b5a27926bfe349791df74990c43f1a428
Marcel Petrick <mail@marcelpetrick.it>
2019-11-11 11:55:28 +0100

    euler021: started implementation; proper divisors can be determined

--- ProjectEuler/problem021_amicableNumbers.py
+++ ProjectEuler/problem021_amicableNumbers.py
@@ -2,7 +2,8 @@
 #
 # Problem 21
 # Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
-# If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+# If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable
+# numbers.
 #
 # For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284.
 # The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
@@ -21,5 +22,26 @@
 #import time
 #import unitttest
 
+def sumOfProperDivisors(number):
+    return sum(computeProperDivisors(number))
 
-#--- test call ---
+def computeProperDivisors(number):
+    if number <= 0:
+        raise ValueError("Only positive integers bigger than zero are allowed.")
+
+    properDivisors = set()
+    for divisor in range(1, number):
+        dividend, modulo = divmod(number, divisor)
+
+        if modulo == 0:
+            properDivisors.add(divisor)
+            properDivisors.add(dividend) # save one call
+
+    return properDivisors
+
+# --- test call ---
+def driver(number):
+    print(number, "-->", computeProperDivisors(number), "-->", sumOfProperDivisors(number))
+
+driver(220)
+driver(1024)
