cede2eb5fe05512f112ded5c3ac369076e1b5c88
Marcel Petrick <mail@marcelpetrick.it>
2019-11-21 22:15:55 +0100

    euler081: added issue

---
+++ ProjectEuler/problem081_pathSumTwoWays.py
@@ -0,0 +1,28 @@
+# Path sum: two ways
+#
+# Problem 81
+# In the 5 by 5 matrix below, the minimal path sum from the top left to the bottom right,
+# by only moving to the right and down, is indicated in bold red and is equal to 2427.
+#
+# ⎛⎝⎜⎜⎜⎜⎜⎜131201630537805673968036997322343427464975241039654221213718150111956331⎞⎠⎟⎟⎟⎟⎟⎟
+# Find the minimal path sum from the top left to the bottom right by only moving right and down in matrix.txt
+# (right click and "Save Link/Target As..."), a 31K text file containing an 80 by 80 matrix.
+
+# ------------------------------------------------------------------------------
+
+# idea:
+# TODO
+
+# ------------------------------------------------------------------------------
+# implementation
+# ------------------------------------------------------------------------------
+
+def function():
+    pass
+
+# ------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    print("bla blubb result", function())
+
+# ------------------------------------------------------------------------------
