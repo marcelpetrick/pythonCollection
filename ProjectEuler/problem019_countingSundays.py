706031f99936dae93b0d3411afeefc03d0188201
Marcel Petrick <mail@marcelpetrick.it>
2019-11-21 22:08:32 +0100

    euler019: cleaned code

--- ProjectEuler/problem019_countingSundays.py
+++ ProjectEuler/problem019_countingSundays.py
@@ -24,20 +24,6 @@
 # implementation
 # ------------------------------------------------------------------------------
 
-import datetime
-datetime.datetime.today()
-#datetime.datetime(2012, 3, 23, 23, 24, 55, 173504)
-print(datetime.datetime.today().weekday())
-
-from datetime import datetime
-from datetime import timedelta
-#Pass multiple parameters (1 day and 5 minutes)
-print("added 1 d 5 min:", datetime.now() + timedelta(days=1, minutes=5))
-print("tomorrow:", (datetime.now() + timedelta(days=1)).weekday())
-
-#
-
-# ------------------------------------------------------------------------------
 def howManySundaysOnFirstOfMonthIn20thCentury():
     from datetime import datetime
     from datetime import timedelta
@@ -53,7 +39,7 @@ def howManySundaysOnFirstOfMonthIn20thCentury():
 
     # increase now the currentDate with 7 day-leaps, then check the day
     while currentDate < datetime.datetime(2000, 12, 31):
-        if currentDate.day() == 1:
+        if currentDate.day == 1:
             amountOfSundaysOnFirstOfMonth -=- 1
             print("found one:", currentDate)
         currentDate += timedelta(days = 7)
