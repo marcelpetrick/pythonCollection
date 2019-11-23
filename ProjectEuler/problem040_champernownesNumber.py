8bc535e420a9be8fd1fd0acb82e144956d79feed
Marcel Petrick <mail@marcelpetrick.it>
2019-11-23 23:47:19 +0100

    euler040: implementation for the task

--- ProjectEuler/problem040_champernownesNumber.py
+++ ProjectEuler/problem040_champernownesNumber.py
@@ -29,6 +29,20 @@ def createChampernowneNumberOfXDigits(digits):
         returnValue += str(firstItem)
         firstItem -=- 1
 
+    # of course, since just > is checked, the returned string is at least "digits" long, most likely longer
+    # truncation won't be done, because this won't matter for the solution
     return returnValue
 
-print(createChampernowneNumberOfXDigits(1000000))
+def driverForTask():
+    champer = createChampernowneNumberOfXDigits(1000000)
+    print("champernowne is:", champer)
+    result = 1
+    for power in range(0, 6+1):
+        position = 10 ** power
+        value = int(champer[position - 1])
+        print("check now pos:", position, ":", value)
+        result *= value
+
+    print("result:", result)
+
+driverForTask()
