8406ad8d5ed642f2e5634f1a4030aae6b7b53fd8
Marcel Petrick <mail@marcelpetrick.it>
2019-11-27 22:56:49 +0100

    uPython: added first copied test program

---
+++ uPython/workSpace/helloWorldOrSo.py
@@ -0,0 +1,9 @@
+# copied from https://randomnerdtutorials.com/getting-started-micropython-esp32-esp8266/
+from machine import Pin
+from time import sleep
+
+led = Pin(2, Pin.OUT)
+
+while True:
+    led.value(not led.value())
+    sleep(0.5)
