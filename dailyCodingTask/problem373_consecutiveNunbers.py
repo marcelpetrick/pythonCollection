29eef33f627112e7ae6a4eb1e5782347ba105df9
Marcel Petrick <mail@marcelpetrick.it>
2019-11-28 23:20:13 +0100

    problem373: added unittest

--- dailyCodingTask/problem373_consecutiveNunbers.py
+++ dailyCodingTask/problem373_consecutiveNunbers.py
@@ -6,3 +6,23 @@
 # For example, given L = [5, 2, 99, 3, 4, 1, 100], return 5 as we can build a sequence [1, 2, 3, 4, 5] which has length 5.
 
 # ------------------------------------------------------------------------------
+
+def longestConsecutiveSequence(inputList):
+    if len(inputList) == 0:
+        raise ValueError("wrong input, just a list please")
+
+    return 1 # todo
+
+# ------------------------------------------------------------------------------
+# proper unit-test
+import unittest
+class Testcase(unittest.TestCase):
+    def test_givenExample(self):
+        giveInput = [5, 2, 99, 3, 4, 1, 100]
+        expectedOutput = 5
+        computedOutput = longestConsecutiveSequence(giveInput)
+        self.assertEqual(expectedOutput, computedOutput)
+
+# ---- here comes the execution of the unit-tests ----
+if __name__ == '__main__':
+    unittest.main()
