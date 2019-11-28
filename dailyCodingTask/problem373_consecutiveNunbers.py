9e09ff772b28cac51a0c2ba887fda9439b370ce2
Marcel Petrick <mail@marcelpetrick.it>
2019-11-28 23:31:40 +0100

    problem373: solved, added own unit-test-cases; fixed a discovered bug for lists with just one element

--- dailyCodingTask/problem373_consecutiveNunbers.py
+++ dailyCodingTask/problem373_consecutiveNunbers.py
@@ -11,7 +11,28 @@ def longestConsecutiveSequence(inputList):
     if len(inputList) == 0:
         raise ValueError("wrong input, just a list please")
 
-    return 1 # todo
+    if len(inputList) == 1:
+        return 1 # shortcut ..
+
+    # sort first ascending
+    inputList.sort()
+    #print(inputList)
+
+    # find the longest sequence by going through the whole list and check for sequences
+    longestSequence = 0
+    currentSequence = 0
+    lastValue = inputList[0] - 1 # init it properly
+    for index in range(len(inputList)):
+        if inputList[index] == lastValue + 1:
+            #print("+1")
+            currentSequence += 1
+        else:
+            #print("fail. was", currentSequence)
+            longestSequence = currentSequence
+            currentSequence = 1
+        lastValue = inputList[index]
+
+    return longestSequence
 
 # ------------------------------------------------------------------------------
 # proper unit-test
@@ -20,6 +41,18 @@ class Testcase(unittest.TestCase):
     def test_givenExample(self):
         giveInput = [5, 2, 99, 3, 4, 1, 100]
         expectedOutput = 5
+        computedOutput = longestConsecutiveSequence(giveInput)
+        self.assertEqual(expectedOutput, computedOutput)
+
+    def test_ownTrack0(self):
+        giveInput = [2, 4, 11, 12, 5, 7, 8, 9]
+        expectedOutput = 3
+        computedOutput = longestConsecutiveSequence(giveInput)
+        self.assertEqual(expectedOutput, computedOutput)
+
+    def test_ownTrack1(self):
+        giveInput = [2]
+        expectedOutput = 1
         computedOutput = longestConsecutiveSequence(giveInput)
         self.assertEqual(expectedOutput, computedOutput)
 
