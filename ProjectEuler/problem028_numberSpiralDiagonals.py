b6a6ab5c47b72321b9f9873834b2db1f0d15eee6
Marcel Petrick <mail@marcelpetrick.it>
2019-12-03 09:55:36 +0100

    problem028: added

---
+++ ProjectEuler/problem028_numberSpiralDiagonals.py
@@ -0,0 +1,45 @@
+# Number spiral diagonals
+#
+# Problem 28
+#
+# Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+#
+# 21 22 23 24 25
+# 20  7  8  9 10
+# 19  6  1  2 11
+# 18  5  4  3 12
+# 17 16 15 14 13
+#
+# It can be verified that the sum of the numbers on the diagonals is 101.
+#
+# What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+
+# ------------------------------------------------------------------------------
+
+# idea:
+# TODO
+
+# ------------------------------------------------------------------------------
+import unittest
+# ------------------------------------------------------------------------------
+
+class Testcase(unittest.TestCase):
+    def test_isPrime(self):
+        self.assertEqual(True, True)
+    #     self.assertEqual(True, isPrime(3))
+    #     self.assertEqual(False, isPrime(6))
+    #     self.assertEqual(True, isPrime(7))
+    #     self.assertEqual(False, isPrime(16))
+    #     self.assertEqual(False, isPrime(25))
+    #     self.assertEqual(True, isPrime(29))
+    #     self.assertEqual(False, isPrime(87))
+    #     self.assertEqual(True, isPrime(89))
+    #
+    # def test_findLargestPrimeFactor(self):
+    #     input = 13195
+    #     expectedResult = 29
+    #     output = findLargestPrimeFactor(input)
+    #     self.assertEqual(output, expectedResult)
+    #     print(" --> input", input, "yielded result:", output)
+
+# ------------------------------------------------------------------------------
