59023aee17498a675d059a9c1ac45e5eda47906d
Marcel Petrick <mail@marcelpetrick.it>
2019-12-10 17:16:32 +0100

    daily coding task: added the challeng to implement some base64-encoding

---
+++ dailyCodingTask/day382_base64Encoding.py
@@ -0,0 +1,20 @@
+# Good morning! Here's your coding interview problem for today.
+#
+# This problem was asked by Google.
+#
+# Yesterday you implemented a function that encodes a hexadecimal string into Base64.
+#
+# Write a function to decode a Base64 string back to a hexadecimal string.
+#
+# For example, the following string:
+#
+# 3q2+7w==
+#
+# should produce:
+#
+# deadbeef
+
+# --------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------
