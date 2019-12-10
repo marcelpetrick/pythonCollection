f977c9f3ef0f78e3a95d81731051da1df43a15f7
Marcel Petrick <mail@marcelpetrick.it>
2019-12-10 17:18:39 +0100

    daily coding task: added 381; fixed 382, because 381 is encoding base64, 382 is decoding

---
+++ dailyCodingTask/day381_base64Encoding.py
@@ -0,0 +1,20 @@
+# Good morning! Here's your coding interview problem for today.
+#
+# This problem was asked by Paypal.
+#
+# Read this Wikipedia article on Base64 encoding. // https://en.wikipedia.org/wiki/Base64
+#
+# Implement a function that converts a hex string to base64.
+#
+# For example, the string:
+#
+# deadbeef
+#
+# should produce:
+#
+# 3q2+7w==
+
+# --------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------
