22d354405807224a094e51b67eea8dae3a12fdd5
Marcel Petrick <mail@marcelpetrick.it>
2019-12-16 22:27:36 +0100

    euler043: added additional needed husks

--- ProjectEuler/problem043_substringDivisibility.py
+++ ProjectEuler/problem043_substringDivisibility.py
@@ -1,7 +1,8 @@
 # Sub-string divisibility
 #
 # Problem 43
-# The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order, but it also has a rather interesting sub-string divisibility property.
+# The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some
+# order, but it also has a rather interesting sub-string divisibility property.
 #
 # Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
 #
@@ -13,3 +14,28 @@
 # d7d8d9=728 is divisible by 13
 # d8d9d10=289 is divisible by 17
 # Find the sum of all 0 to 9 pandigital numbers with this property.
+#
+# ------------------------------------------------------------------------------
+# idea:
+#
+#
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# implementation
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# unit test
+# ------------------------------------------------------------------------------
+import unittest
+class Testcase(unittest.TestCase):
+
+    def test_whatever(self):
+        #self.assertEqual(36, computeAmountOfDivisors(3658732))
+        pass
+
+# --- test call
+#getFirstTriangleNumberWithMoreThanXDivisors(500)
