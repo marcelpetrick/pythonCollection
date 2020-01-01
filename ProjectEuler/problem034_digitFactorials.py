9ec1df99e918981a352b8fbf7184cd5408572eb0
Marcel Petrick <mail@marcelpetrick.it>
2020-01-01 23:50:46 +0100

    euler034: added hints

--- ProjectEuler/problem034_digitFactorials.py
+++ ProjectEuler/problem034_digitFactorials.py
@@ -6,3 +6,15 @@
 # Find the sum of all numbers which are equal to the sum of the factorial of their digits.
 #
 # Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+
+# ------------------------------------------------------------------------------
+# idea:
+# * a function to compute the factorial (maybe with dictionary for caching) - not really needed, because just 0 to 9
+# * if one of the facs is bigger than the wanted sum, then it can be skipped immediately, or?
+# * what is the boundary? how to know to stop?
+#
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# implementation
+# ------------------------------------------------------------------------------
