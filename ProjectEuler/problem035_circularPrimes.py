2f9bc1dea8c5171bcbf39a1a052aebfa0ac0546c
Marcel Petrick <mail@marcelpetrick.it>
2020-01-12 16:40:24 +0100

    euler035: implementation

--- ProjectEuler/problem035_circularPrimes.py
+++ ProjectEuler/problem035_circularPrimes.py
@@ -15,6 +15,44 @@
 # * return len(resultList) .. profit!
 
 # ------------------------------------------------------------------------------
+# implementation
+# ------------------------------------------------------------------------------
 
+# reuse code from previous solution
+# TODO maybe consider to use a prime-generator, which is proven and tested; see problem041
+def getPrimesUntilLimit(limit):
+    from ProjectEuler.problem668_numpy import sieveEras  # works, but just after commenting lots of code inside that file
+    primes = sieveEras(limit, False) # this is also a mistake in the second parameter
+    #print(len(primes), ":", primes)  # 78,498 for 10 ** 6 - which is correct
+
+    return primes
 
 # ------------------------------------------------------------------------------
+
+import itertools
+def isGivenPrimeACircularPrime(prime, primeList):
+    # checks if prime is really a prime or if primeList is not empty are omitted for performance reasons!
+    primeString = str(prime)
+    for permutationTuple in itertools.permutations(primeString):
+        permutAsNumber = int(''.join(permutationTuple))
+        #print("current permut:", permutAsNumber)
+        if permutAsNumber not in primeList:
+            return False
+
+    return True
+
+# ------------------------------------------------------------------------------
+
+def createListOfCircularPrimesBelowLimit(limit):
+    # generate the primes
+    primes = getPrimesUntilLimit(limit)
+
+    # filter the prime list by the circular-function
+    circularPrimeList = [elem for elem in primes[:] if isGivenPrimeACircularPrime(elem, primes)]
+
+    return circularPrimeList
+
+# ------------------------------------------------------------------------------
+
+# expected: 100 -> 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+print(createListOfCircularPrimesBelowLimit(10 ** 6))
