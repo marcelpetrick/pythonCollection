42d9b607a52f439d6e426fba3cdd1d407432f1d1
Marcel Petrick <mail@marcelpetrick.it>
2020-01-12 16:29:38 +0100

    problem035: added task and idea

---
+++ ProjectEuler/problem035_circularPrimes.py
@@ -0,0 +1,20 @@
+# Circular primes
+#
+# Problem 35
+# The number, 197, is called a circular prime because all rotations of the digits: 197, 971, and 719,
+# are themselves prime.
+#
+# There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+#
+# How many circular primes are there below one million?
+
+# ------------------------------------------------------------------------------
+# idea:
+# * compute all primes up to the limit, take the function from problem 50
+# * create function which returns if a given prime is a circular prime (permutation..)
+# * return len(resultList) .. profit!
+
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
