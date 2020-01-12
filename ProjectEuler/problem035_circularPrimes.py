e5bb6ac2731a41ac51d99f30dc17e1560e6a722a
Marcel Petrick <mail@marcelpetrick.it>
2020-01-12 17:24:33 +0100

    euler035: added the handed in solution

--- ProjectEuler/problem035_circularPrimes.py
+++ ProjectEuler/problem035_circularPrimes.py
@@ -87,3 +87,37 @@ for i in range(1, 7):
 # print("result 10.000:", result)
 
 # ------------------------------------------------------------------------------
+
+# #### run for 10 ** 1 ####
+# made primesUpToLimit
+# 0.20301008224487305 s; exp =  1 result =  [2, 3, 5, 7]
+# len: 4 first prime is: 2 sum is: 17
+# #### run for 10 ** 2 ####
+# made primesUpToLimit
+# 0.0 s; exp =  2 result =  [2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97]
+# len: 13 first prime is: 2 sum is: 446
+# #### run for 10 ** 3 ####
+# made primesUpToLimit
+# 0.0029976367950439453 s; exp =  3 result =  [2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97, 113, 131, 197, 199, 311, 337, 373, 719, 733, 919, 971, 991]
+# len: 25 first prime is: 2 sum is: 6440
+# #### run for 10 ** 4 ####
+# made primesUpToLimit
+# 0.038007259368896484 s; exp =  4 result =  [2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97, 113, 131, 197, 199, 311, 337, 373, 719, 733, 919, 971, 991, 1193, 1931, 3119, 3779, 7793, 7937, 9311, 9377]
+# len: 33 first prime is: 2 sum is: 50880
+# #### run for 10 ** 5 ####
+# made primesUpToLimit
+# 1.2425141334533691 s; exp =  5 result =  [2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97, 113, 131, 197, 199, 311, 337, 373, 719, 733, 919, 971, 991, 1193, 1931, 3119, 3779, 7793, 7937, 9311, 9377, 11939, 19391, 19937, 37199, 39119, 71993, 91193, 93719, 93911, 99371]
+# len: 43 first prime is: 2 sum is: 628652
+# #### run for 10 ** 6 ####
+# made primesUpToLimit
+# 92.9756772518158 s; exp =  6 result =  [2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97, 113, 131, 197, 199, 311, 337, 373, 719, 733, 919, 971, 991, 1193, 1931, 3119, 3779, 7793, 7937, 9311, 9377, 11939, 19391, 19937, 37199, 39119, 71993, 91193, 93719, 93911, 99371, 193939, 199933, 319993, 331999, 391939, 393919, 919393, 933199, 939193, 939391, 993319, 999331]
+# len: 55 first prime is: 2 sum is: 8184200
+#
+# Process finished with exit code 0
+
+# ------------------------------------------------------------------------------
+# Congratulations, the answer you gave to problem 35 is correct.
+#
+# You are the 79351st person to have solved this problem.
+#
+# This problem had a difficulty rating of 5%. The highest difficulty rating you have solved so far is 25%.
