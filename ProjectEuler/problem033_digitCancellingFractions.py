25443fa0e030be9044e1a5cd79823f50633e2bb5
Marcel Petrick <mail@marcelpetrick.it>
2020-01-30 23:51:33 +0100

    euler033: final version; done

--- ProjectEuler/problem033_digitCancellingFractions.py
+++ ProjectEuler/problem033_digitCancellingFractions.py
@@ -13,7 +13,7 @@
 
 # ------------------------------------------------------------------------------
 
-def isFakeRoundable(numA, numB):
+def isFakeCancellable(numA, numB):
     strA = str(numA)
     strB = str(numB)
 
@@ -39,15 +39,15 @@ def isFakeRoundable(numA, numB):
     return False
 
 # ------------------------------------------------------------------------------
-print("21/13:", isFakeRoundable(21, 13))
-print("49/98:", isFakeRoundable(49, 98))
+print("21/13:", isFakeCancellable(21, 13))
+print("49/98:", isFakeCancellable(49, 98))
 # ------------------------------------------------------------------------------
 
 results = []
 for denominator in range(10, 100): # since the resulting number shall be below 1
     for numerator in range(10, denominator+1):
         print("check now:", denominator, numerator)
-        if isFakeRoundable(numerator, denominator):
+        if isFakeCancellable(numerator, denominator):
             if numerator != denominator:
                 results.append([numerator, denominator])
 
@@ -60,3 +60,22 @@ print("results:", results)
 # one cancellable: a
 # fake cancel is equal to real result! 1.0 == 1.0
 # results: [[16, 64], [26, 65], [19, 95], [49, 98]]
+# ------------------------------------------------------------------------------
+
+# See: "If the product of these four fractions is given in its lowest common terms, find the value of the denominator."
+from fractions import Fraction
+fractionProduc = Fraction(1,1) # identity
+for elem in results:
+    fractionProduc *= Fraction(elem[0], elem[1])
+print("final product:", fractionProduc, "==>", fractionProduc.denominator)
+# ------------------------------------------------------------------------------
+# ..
+# results: [[16, 64], [26, 65], [19, 95], [49, 98]]
+# final product: 1/100 ==> 100
+# ------------------------------------------------------------------------------
+# Congratulations, the answer you gave to problem 33 is correct.
+#
+# You are the 67196th person to have solved this problem.
+#
+# This problem had a difficulty rating of 5%. The highest difficulty rating you have solved so far is 25%.
+# ------------------------------------------------------------------------------
