046036d827b4b6daa66ed6d7cebc6459e316eee8
Marcel Petrick <mail@marcelpetrick.it>
2020-02-23 22:46:05 +0100

    euler063: added task

---
+++ ProjectEuler/problem063_powerfulDigitCounts.py
@@ -0,0 +1,20 @@
+# Powerful digit counts
+#
+# Problem 63
+# The 5-digit number, 16807=75, is also a fifth power. Similarly, the 9-digit number, 134217728=89, is a ninth power.
+#
+# How many n-digit positive integers exist which are also an nth power?
+# ------------------------------------------------------------------------------
+# idea:
+# TODO
+#
+#
+# ------------------------------------------------------------------------------
+
+#import unittest
+
+# # ---- here comes the execution of the unit-tests ----
+# if __name__ == '__main__':
+#     unittest.main()
+
+# ------------------------------------------------------------------------------
