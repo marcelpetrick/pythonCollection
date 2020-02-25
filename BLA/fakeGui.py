85cdcb252c47f47f9f7693b31a3824884e9955f2
Marcel Petrick <mail@marcelpetrick.it>
2020-02-25 22:05:46 +0100

    BLA: first example done

---
+++ BLA/fakeGui.py
@@ -0,0 +1,23 @@
+# follows https://build-system.fman.io/pyqt5-tutorial
+
+# upgrade pip:
+# $ python -m pip install --upgrade pip
+# install PyQt in the latest version: 5.14 for me
+# pip install PyQt5
+
+#------------------------------------------------------
+# description: first simple example
+# from PyQt5.QtWidgets import QApplication, QLabel
+#
+# app = QApplication([])
+# label = QLabel("ja, super")
+# label.show()
+# app.exec_()
+#------------------------------------------------------
+
+
+#------------------------------------------------------
+
+#------------------------------------------------------
+
+#------------------------------------------------------
