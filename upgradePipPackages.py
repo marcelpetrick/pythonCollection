428d8ddd2300fa562b725133d67a80952f3d99e5
Marcel Petrick <mail@marcelpetrick.it>
2020-03-02 13:16:31 +0100

    upgrade all installed pip packages: via python script (taken from: https://stackoverflow.com/a/5839291/1694302 ) Works on Win10.

---
+++ upgradePipPackages.py
@@ -0,0 +1,7 @@
+# taken from: https://stackoverflow.com/a/5839291/1694302
+
+import pkg_resources
+from subprocess import call
+
+packages = [dist.project_name for dist in pkg_resources.working_set]
+call("pip install --upgrade " + ' '.join(packages), shell=True)
