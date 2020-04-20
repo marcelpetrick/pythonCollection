dedf4ec190b62267ec270542753780f9912505e0
Marcel Petrick <mail@marcelpetrick.it>
2020-04-20 18:01:17 +0200

    compound interest: works; yields the expected result for 10 ears 500e and 10%

--- financeCalender/compoundInterestPreview.py
+++ financeCalender/compoundInterestPreview.py
@@ -2,17 +2,27 @@
 # 12 months per year are assumed
 
 initialAmount = 0 # in Euro
-ratePerMonth = 200 # in Euro; added twelve times
+ratePerMonth = 500 # in Euro; added twelve times
 forecastDuration = 10 # in years
 expectedInterest = 10 # in percent; is added once at the end of every year; not per month!
 
 def determineFinalAmount():
 
+    currentAmount = initialAmount
     for year in range(0, forecastDuration):
         print("year " + str(year+1) + ":")
+        # invested
+        for month in range(0, 12):
+            currentAmount += ratePerMonth
 
+        # gathered by interest (attention: problem is that this is calculated on the full amount, which is wrong, slightly, but wrong)
+        currentAmount = (1.0 + expectedInterest / 100.0) * currentAmount
 
-
+        print(str(currentAmount) + "€")
 
 # test call
 determineFinalAmount()
+
+# result:
+# year 10:
+# 105187.00236660003€
