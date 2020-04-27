103859f8d669461bc2313cfadb643ae888e09655
Marcel Petrick <mail@marcelpetrick.it>
2020-04-28 00:33:55 +0200

    bla: unzip-test-implementation

--- BLA/bla.py
+++ BLA/bla.py
@@ -3,3 +3,14 @@
 # get for each warning the cause as equivalence-class
 # remove the unzipped directory
 # print final statistics
+
+def unzip(zipFilePath, unzipPath):
+    from zipfile import ZipFile
+
+    with ZipFile(zipFilePath, 'r') as zipObject:
+        zipObject.extractall(unzipPath)
+
+    pass
+
+
+unzip("test.zip", "extractDir")
