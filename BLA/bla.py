d8038fd1f9a37a8e8fa069763eeab066e693f62f
Marcel Petrick <mail@marcelpetrick.it>
2020-04-28 00:29:12 +0200

    bla: scribbled down the strategy

---
+++ BLA/bla.py
@@ -0,0 +1,5 @@
+# extract zip archive to self-chosen directory
+# parse its content for "warning:"
+# get for each warning the cause as equivalence-class
+# remove the unzipped directory
+# print final statistics
