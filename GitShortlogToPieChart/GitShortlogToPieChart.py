547eaeb5dee7143167aeb17bdd1ac445b2110129
Marcel Petrick <mail@marcelpetrick.it>
2020-06-26 17:00:34 +0200

    GitShortlogToPieChart: working plotting-method

--- GitShortlogToPieChart/GitShortlogToPieChart.py
+++ GitShortlogToPieChart/GitShortlogToPieChart.py
@@ -7,8 +7,8 @@ def generateExampleChart():
         # and: https://matplotlib.org/3.1.1/gallery/pie_and_polar_charts/pie_and_donut_labels.html#sphx-glr-gallery-pie-and-polar-charts-pie-and-donut-labels-py
 
         # Pie chart, where the slices will be ordered and plotted counter-clockwise:
-        labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-        sizes = [15, 30, 45, 10]
+        labels = 'Frogs', 'Hogs', 'Dogs', 'Logs' # list
+        sizes = [15, 30, 45, 10] # array
         #explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
         fig1, ax1 = plt.subplots()
@@ -25,16 +25,47 @@ def generateExampleChart():
 #-------------------------
 
 def prepareFileContent():
-     with open('testdata.csv') as file:
-             for line in file.readlines():
-                # trim it from whitespace
-                strippedLine = line.strip()
-                print(strippedLine) # todom remove
-                # split at first occasion of " "
-             
+        results = dict()
+        with open('testdata.csv') as file:
+                for line in file.readlines():
+                        # trim it from whitespace
+                        strippedLine = line.strip()
+                        #print(strippedLine)  # todom remove
+                        # split at first occasion of " "
+                        amount, name = strippedLine.split(' ', 1)
+                        #print(name, amount)  # todom remove
+                        # just take the name, not the mail - else the legend is too long
+                        key = list(name.split('<'))[0].rstrip()
+                        results[key] = amount
 
+        return results
 
-prepareFileContent()
+#-------------------------
+
+def renderPieChart(fileContentDict):
+        print(fileContentDict) # todom  remove
+
+        # data preparation
+        labels = fileContentDict.keys()
+        sizes = fileContentDict.values()
+
+        # chart creation
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes,
+                #explode=explode,
+                labels=labels,
+                autopct='%1.1f%%',
+                shadow=False,
+                startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+        plt.show()
+
+#-------------------------
+
+fileContentDict = prepareFileContent()
+renderPieChart(fileContentDict)
+
 #-------------------------
 #-------------------------
 #-------------------------
