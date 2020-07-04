3ec8a1fc8325d066d979ab3140bd6f524032c238
Marcel Petrick <mail@marcelpetrick.it>
2020-07-04 11:33:12 +0200

    fakeGui: connecting slider to lcd-display works. But should be done inside a class ..

--- PyQt_learning/fakeGui.py
+++ PyQt_learning/fakeGui.py
@@ -1,3 +1,8 @@
+# author mail@marcelpetrick.it
+# license: GPLv3.0
+
+#------------------------------------------------------
+
 # upgrade pip:
 # $ python -m pip install --upgrade pip
 # install PyQt in the latest version: 5.14 for me
@@ -18,24 +23,43 @@
 # app.exec_()
 #------------------------------------------------------
 
-# description: two buttons in a layout
-from PyQt5.QtWidgets import QApplication\
-    , QWidget\
-    , QPushButton\
-    , QVBoxLayout \
-    , QSpacerItem
+def firstPyQtGui():
+    # description: two buttons in a layout
+    from PyQt5.QtWidgets import QApplication\
+        , QWidget\
+        , QPushButton \
+        , QVBoxLayout \
+        , QSpacerItem \
+        , QSpacerItem \
+        , QLCDNumber \
+        , QSlider
+    from PyQt5.QtCore import Qt # for the orientation
 
-app = QApplication([])
-app.setApplicationName("my second PyQt app, lol")
-window = QWidget()
-layout = QVBoxLayout()
-layout.addWidget(QPushButton("upper button"))
-layout.addWidget(QPushButton("lower button"))
-spacer = QSpacerItem(666, 1)
-layout.addSpacerItem(spacer)
-window.setLayout(layout)
-window.show()
-app.exec() # what is the difference to exec()?
+    app = QApplication([])
+    app.setApplicationName("my second PyQt app, lol")
+    window = QWidget()
+    layout = QVBoxLayout()
+
+    # add the widgets
+    upperButton = QPushButton("upper button")
+    layout.addWidget(upperButton)
+    lowerButton = QPushButton("lower button")
+    layout.addWidget(lowerButton)
+
+    lcd = QLCDNumber() # research: how to hand over the parent for widget at PyQt?
+    layout.addWidget(lcd)
+    #upperButton.clicked.connect(lcd.display("1"))
+    slider = QSlider(Qt.Horizontal)
+    layout.addWidget(slider)
+    slider.valueChanged.connect(lcd.display)
+
+    # just make it a bit wider -  else the window's title is cut
+    spacer = QSpacerItem(666, 1)
+    layout.addSpacerItem(spacer)
+    window.setLayout(layout)
+    # execute
+    window.show()
+    app.exec() # what is the difference to exec()?
 
 #------------------------------------------------------
 
@@ -47,3 +71,12 @@ app.exec() # what is the difference to exec()?
 # https://machinekoder.com/pyqt-vs-qt-for-python-pyside2-pyside/
 
 #------------------------------------------------------
+
+
+#-------------------------
+
+# ### execution ###
+if __name__ == "__main__":
+    firstPyQtGui()
+
+#-------------------------
