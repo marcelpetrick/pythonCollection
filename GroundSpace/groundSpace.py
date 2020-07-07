92d87c859e39e5d758ee76c8cf09b807277bc1ec
Marcel Petrick <mail@marcelpetrick.it>
2020-07-07 17:29:40 +0200

    groundSpace: bit optimized writing; bigger chunk (but not too big); bigger loop

--- GroundSpace/groundSpace.py
+++ GroundSpace/groundSpace.py
@@ -6,16 +6,18 @@ def groundSpace():
 
     #print("before writing")
     #listOfItems = [123, 124, 64, 98, 12]
-    listOfItems = [0] * 1024
+    listOfItems = [0] * (2 ** 20)
     byteArray = bytearray(listOfItems)
     with open('temporaryFile.tmp', 'ba') as tempFile: # refer to this for the second param: https://docs.python.org/3/library/functions.html#open
-        for a in range(2 ** 10):
+        for a in range(2 ** 30):
             tempFile.write(byteArray)
+            print("one loop-block done")
 
     #print("done writing")
 
 #-----------------------------
 
 # I am impressed: writes 8 GiB in some seconds ... can be maybe improved
+# Samsung SSD 960 EVO 500GB achieves just 300 MB/s continuous
 while True:
     groundSpace()
