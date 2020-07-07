86eff4806519b0d488d812cbb89a81e0967ffad0
Marcel Petrick <mail@marcelpetrick.it>
2020-07-07 17:21:29 +0200

    groundSpace: improved the workflow; write more data; just write Zeroes (recommended); repeat until interruption

--- GroundSpace/groundSpace.py
+++ GroundSpace/groundSpace.py
@@ -4,14 +4,18 @@
 
 def groundSpace():
 
-    print("before writing")
-    listOfItems = [123, 124, 64, 98, 12]
+    #print("before writing")
+    #listOfItems = [123, 124, 64, 98, 12]
+    listOfItems = [0] * 1024
     byteArray = bytearray(listOfItems)
-    with open('temporaryFile.tmp', 'w+b') as tempFile:
-        tempFile.write(byteArray)
+    with open('temporaryFile.tmp', 'ba') as tempFile: # refer to this for the second param: https://docs.python.org/3/library/functions.html#open
+        for a in range(2 ** 10):
+            tempFile.write(byteArray)
 
-    print("done writing")
+    #print("done writing")
 
 #-----------------------------
 
-groundSpace()
+# I am impressed: writes 8 GiB in some seconds ... can be maybe improved
+while True:
+    groundSpace()
