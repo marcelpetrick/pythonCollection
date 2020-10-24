e5c06ad921ede237c4c899bbe01e66e51c652260
Marcel Petrick <mail@marcelpetrick.it>
2020-10-24 23:58:27 +0200

    proejct euler 039: added unit-test

--- ProjectEuler/problem039_integerRightTriangles.py
+++ ProjectEuler/problem039_integerRightTriangles.py
@@ -30,11 +30,22 @@ def checkTriangleForBeingRightAngled(a,b,c):
     return isRightAngled
 
 # ------------------------------------------------------------------------------
+# print(checkTriangleForBeingRightAngled(20, 48, 52))
+# print(checkTriangleForBeingRightAngled(20, 50, 50))
+# ------------------------------------------------------------------------------
 
-if __name__ == "__main__":
-    print("bla blubb result", function())
+import unittest
+class Testcase(unittest.TestCase):
+    def test_isPrime(self):
+        self.assertEqual(True, checkTriangleForBeingRightAngled(20, 48, 52))
+        self.assertEqual(True, checkTriangleForBeingRightAngled(24, 45, 51))
+        self.assertEqual(True, checkTriangleForBeingRightAngled(30, 40, 50))
+        self.assertEqual(False, checkTriangleForBeingRightAngled(30, 42, 48))
 
 # ------------------------------------------------------------------------------
 
-print(checkTriangleForBeingRightAngled(20, 48, 52))
-print(checkTriangleForBeingRightAngled(20, 50, 50))
+if __name__ == '__main__':
+    unittest.main()
+
+# ------------------------------------------------------------------------------
+
