c8ceb3c34413c55141df24e71a783deff7ffa7e3
Marcel Petrick <mail@marcelpetrick.it>
2020-10-25 00:08:30 +0200

    proejct euler 039: continued

--- ProjectEuler/problem039_integerRightTriangles.py
+++ ProjectEuler/problem039_integerRightTriangles.py
@@ -18,20 +18,31 @@
 # implementation
 # ------------------------------------------------------------------------------
 
-def function():
-    pass
-
-# ------------------------------------------------------------------------------
-
-def checkTriangleForBeingRightAngled(a,b,c):
+def checkTriangleForBeingRightAngled(a, b, c):
     # find the biggest by creating a sorted list
-    sl = sorted([a,b,c])
+    sl = sorted([a, b, c])
     isRightAngled = sl[0] ** 2 + sl[1] ** 2 == sl[2] ** 2
     return isRightAngled
 
 # ------------------------------------------------------------------------------
 # print(checkTriangleForBeingRightAngled(20, 48, 52))
 # print(checkTriangleForBeingRightAngled(20, 50, 50))
+# ------------------------------------------------------------------------------
+
+def findAllRightAngledPartitionsOfPerimeter(perimeter):
+    result = [] #set()
+    for a in range(1, perimeter - 1): # edges cant be zero, so from 1 to perimeter-2
+        for b in range(1, perimeter - a):
+            c = perimeter - a - b
+            # note: this simple approach also creates partitions like 1,2,3 and 1,3,2 for 6 - which are identical and therefore don't have to be checked multiple times ..
+            #print(a,b,c, "=", a+b+c)
+
+            if(checkTriangleForBeingRightAngled(a, b, c)):
+                #result.add(';'.join(sorted([a, b, c])))
+                result.append([a, b, c])
+
+    print(result)
+findAllRightAngledPartitionsOfPerimeter(120)
 # ------------------------------------------------------------------------------
 
 import unittest
