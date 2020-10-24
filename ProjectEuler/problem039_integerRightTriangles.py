5bdf6936ea5ac2eb704ed19b552b26e76f0078f4
Marcel Petrick <mail@marcelpetrick.it>
2020-10-25 00:19:52 +0200

    proejct euler 039: continued

--- ProjectEuler/problem039_integerRightTriangles.py
+++ ProjectEuler/problem039_integerRightTriangles.py
@@ -30,7 +30,7 @@ def checkTriangleForBeingRightAngled(a, b, c):
 # ------------------------------------------------------------------------------
 
 def findAllRightAngledPartitionsOfPerimeter(perimeter):
-    result = [] #set()
+    result = set()
     for a in range(1, perimeter - 1): # edges cant be zero, so from 1 to perimeter-2
         for b in range(1, perimeter - a):
             c = perimeter - a - b
@@ -38,21 +38,61 @@ def findAllRightAngledPartitionsOfPerimeter(perimeter):
             #print(a,b,c, "=", a+b+c)
 
             if(checkTriangleForBeingRightAngled(a, b, c)):
-                #result.add(';'.join(sorted([a, b, c])))
-                result.append([a, b, c])
+                result.add(tuple(sorted([a, b, c])))
 
-    print(result)
-findAllRightAngledPartitionsOfPerimeter(120)
+    #print(perimeter, " -->", result)
+    return result
+
+# ------------------------------------------------------------------------------
+
+# import time
+# currentTime = time.time()
+# findAllRightAngledPartitionsOfPerimeter(120)
+# print("computing all right-angled partitions of 120 took", time.time() - currentTime, "s")
+# computing all right-angled partitions of 120 took 0.0 s
+# ------------------------------------------------------------------------------
+
+def findMaxPartitions(inclusiveLimit):
+    # init
+    maxLen = -1
+    bestPerimeter = -1
+
+    # loop
+    for perimeter in range(3, inclusiveLimit + 1):
+        amountResults = len(findAllRightAngledPartitionsOfPerimeter(perimeter))
+
+        if amountResults > maxLen:
+            maxLen = amountResults
+            bestPerimeter = perimeter
+
+    result = findAllRightAngledPartitionsOfPerimeter(bestPerimeter)
+    print("bst perimeter would be ", bestPerimeter, " with ", len(result), " --> ", result)
+
+# ------------------------------------------------------------------------------
+
+import time
+currentTime = time.time()
+findMaxPartitions(1000)
+print("computing the euler-result took", time.time() - currentTime, "s")
+#
+
 # ------------------------------------------------------------------------------
 
 import unittest
 class Testcase(unittest.TestCase):
-    def test_isPrime(self):
+    def test_isRightAngled(self):
         self.assertEqual(True, checkTriangleForBeingRightAngled(20, 48, 52))
         self.assertEqual(True, checkTriangleForBeingRightAngled(24, 45, 51))
         self.assertEqual(True, checkTriangleForBeingRightAngled(30, 40, 50))
         self.assertEqual(False, checkTriangleForBeingRightAngled(30, 42, 48))
 
+    def test_properPartitions(self):
+        testSet = set()
+        testSet.add((20, 48, 52))
+        testSet.add((24, 45, 51))
+        testSet.add((30, 40, 50))
+
+        self.assertEqual(testSet, findAllRightAngledPartitionsOfPerimeter(120))
 # ------------------------------------------------------------------------------
 
 if __name__ == '__main__':
