310cee8554c84a3a3edde9c9cfcb34a54bb3821b
Marcel Petrick <mail@marcelpetrick.it>
2020-10-24 23:35:02 +0200

    project euler 039: continued

--- ProjectEuler/problem039_integerRightTriangles.py
+++ ProjectEuler/problem039_integerRightTriangles.py
@@ -2,11 +2,12 @@
 #
 # Problem 39
 #
-# If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
+# If p is the perimeter of a right angle triangle with integral length sides, {a,b,c},
+# there are exactly three solutions for p = 120.
 #
 # {20,48,52}, {24,45,51}, {30,40,50}
 #
-# For which value of p ≤ 1000, is the number of solutions maximised
+# For which value of p ≤ 1000, is the number of solutions maximised?
 
 # ------------------------------------------------------------------------------
 
@@ -22,7 +23,18 @@ def function():
 
 # ------------------------------------------------------------------------------
 
+def checkTriangleForBeingRightAngled(a,b,c):
+    # find the biggest by creating a sorted list
+    sl = sorted([a,b,c])
+    isRightAngled = sl[0] ** 2 + sl[1] ** 2 == sl[2] ** 2
+    return isRightAngled
+
+# ------------------------------------------------------------------------------
+
 if __name__ == "__main__":
     print("bla blubb result", function())
 
 # ------------------------------------------------------------------------------
+
+print(checkTriangleForBeingRightAngled(20, 48, 52))
+print(checkTriangleForBeingRightAngled(20, 50, 50))
