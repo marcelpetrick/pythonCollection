cf49b019f150e59ade8bf100cd409c1ef8d2e701
Marcel Petrick <mail@marcelpetrick.it>
2020-10-29 14:53:18 +0100

    project euler 043: continued implementation

--- ProjectEuler/problem043_subStringDivisibility_differentApproach.py
+++ ProjectEuler/problem043_subStringDivisibility_differentApproach.py
@@ -45,8 +45,8 @@ def hasUniqueDigits(number):
     return everycharUnique
 
 # ------------------------------------------------------------------------------
-print(hasUniqueDigits(221)) # todo convert to unit-test!
-print(hasUniqueDigits(123))
+# print(hasUniqueDigits(221)) # todo convert to unit-test!
+# print(hasUniqueDigits(123))
 # ------------------------------------------------------------------------------
 
 def generateAlld8d9d10Numbers():
@@ -66,22 +66,93 @@ def generateAlld8d9d10Numbers():
 
 # ------------------------------------------------------------------------------
 
-import time
-currentTime = time.time()
-generateAlld8d9d10Numbers()
-print("generation took", time.time() - currentTime, "s")
+# import time
+# currentTime = time.time()
+# generateAlld8d9d10Numbers()
+# print("generation took", time.time() - currentTime, "s")
 
 # ------------------------------------------------------------------------------
 
-def createAllCandidatesWhichFulfilld7d8d9Attribute(inputListOfStringifiedSuffixes):
-    print("continue implementation here")
+def findRemainingDigits(inputString):
+    # gimme all the list comprehensions! maybe the double str(elem) can be eliminated somehow?
+    resultSet = set(str(elem) for elem in range(10) if str(elem) not in inputString)
+    #print(resultSet) # todo remove
+    return resultSet
 
+# ------------------------------------------------------------------------------
+# just a test-call for the function
+# print(findRemainingDigits("123"))
+# print(findRemainingDigits("085"))
+# ------------------------------------------------------------------------------
+
+def createAllCandidatesWhichFulfilld7d8d9Attribute(inputListOfStringifiedSuffixes):
     # 0. create all possible combinations of one element of the  remaining digits prefixed to
     # the given element from inputListOfStringifiedSuffixes
     # 1. then check if this is dividable by 13 (should eliminate a lot of elements)
+
+    for d8d9d10 in inputListOfStringifiedSuffixes:
+        # 0. remaining digits
+        remainingDigits = findRemainingDigits(d8d9d10)
+        for d7 in remainingDigits:
+            d7d8d9d10 = d7 + d8d9d10
+            if int(d7d8d9d10[:3]) % 13 == 0:
+                print("-->", d7d8d9d10)
+
+                # todo continue with that one
+                print("continue implementation here: that block should be refactored out: because it is applied iteratively for all other requirements")
+
     pass
 
 # ------------------------------------------------------------------------------
 
-createAllCandidatesWhichFulfilld7d8d9Attribute()
+createAllCandidatesWhichFulfilld7d8d9Attribute(generateAlld8d9d10Numbers()) # todo make this a better approach; the function shall create the input itself
 
+# ------------------------------------------------------------------------------
+
+# new, optimized function
+
+def createListOfStringifiedNumbersByPrependingOneOfTheRemainingDigitsWhileFulfillingDivisibility(
+        inputListOfStringifiedSuffixes, divisorToObey):
+
+    resultList = []
+
+    for givenSuffixString in inputListOfStringifiedSuffixes:
+        remainingDigits = findRemainingDigits(givenSuffixString)
+        for prefix in remainingDigits:
+            newNumberString = prefix + givenSuffixString
+            if int(newNumberString[:3]) % divisorToObey == 0:
+                #print("-->", newNumberString) # todo remove
+                resultList.append(newNumberString)
+
+    return resultList
+
+# ------------------------------------------------------------------------------
+
+result13 = createListOfStringifiedNumbersByPrependingOneOfTheRemainingDigitsWhileFulfillingDivisibility(generateAlld8d9d10Numbers(), 13)
+print("result13:", result13)
+result11 = createListOfStringifiedNumbersByPrependingOneOfTheRemainingDigitsWhileFulfillingDivisibility(result13, 11)
+print("result11:", result11)
+result7 = createListOfStringifiedNumbersByPrependingOneOfTheRemainingDigitsWhileFulfillingDivisibility(result11, 7)
+print(f"{result7=}") # abused the f-string-functionality for printing the vaiables name in front of the value
+result5 = createListOfStringifiedNumbersByPrependingOneOfTheRemainingDigitsWhileFulfillingDivisibility(result7, 5)
+print(f"{result5=}")
+result3 = createListOfStringifiedNumbersByPrependingOneOfTheRemainingDigitsWhileFulfillingDivisibility(result5, 3)
+print(f"{result3=}")
+result2 = createListOfStringifiedNumbersByPrependingOneOfTheRemainingDigitsWhileFulfillingDivisibility(result3, 2)
+print(f"{result2=}")
+# a filler for the first digit is needed: and this can be whatevery, so just make it divisiable by one!
+result1 = createListOfStringifiedNumbersByPrependingOneOfTheRemainingDigitsWhileFulfillingDivisibility(result2, 1)
+print(f"{result1=}")
+
+finalSum = sum([int(elem) for elem in result1])
+print("send to PE:", finalSum)
+# ------------------------------------------------------------------------------
+
+# result13: ['2085', '9102', '0136', '7153', '5204', '9238', '7289', '1306', '2340', '6374', '0391', '8459', '2476', '3510', '0527', '4680', '1697', '8714', '2731', '0782', '4816', '2867', '3901', '0918', '7935', '1952', '5986']
+# result11: ['89102', '67153', '35204', '79238', '57289', '91306', '80391', '92476', '93510', '60527', '94680', '62731', '40782', '74816', '52867', '53901', '20918', '31952', '75986']
+# result7=['735204', '679238', '357289', '791306', '280391', '392476', '693510', '294680', '462731', '140782', '574816', '952867', '420918', '175986']
+# result5=['6357289', '0357289', '4357289', '1357289', '4952867', '0952867', '3952867', '1952867']
+# result3=['06357289', '60357289', '30952867', '03952867']
+# result2=['406357289', '106357289', '460357289', '160357289', '430952867', '130952867']
+# result1=['1406357289', '4106357289', '1460357289', '4160357289', '1430952867', '4130952867']
+# send to PE: 16695334890
