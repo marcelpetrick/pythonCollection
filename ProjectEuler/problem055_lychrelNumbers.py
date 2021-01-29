da49a31adef0a0737ac0ac7c4e1fb3b5d66382b6
Marcel Petrick <mail@marcelpetrick.it>
2021-01-30 00:46:55 +0100

    project euler 055: approved solution, yeah

--- ProjectEuler/problem055_lychrelNumbers.py
+++ ProjectEuler/problem055_lychrelNumbers.py
@@ -134,3 +134,9 @@ print(f"computation too {time.time() - startTime} seconds")
 # computation too 0.03774762153625488 seconds
 
 # ------------------------------------------------------------------------------
+
+# Congratulations, the answer you gave to problem 55 is correct.
+#
+# You are the 53220th person to have solved this problem.
+#
+# This problem had a difficulty rating of 5%. The highest difficulty rating you have solved so far is 25%.
