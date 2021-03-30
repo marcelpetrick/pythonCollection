654a1bbe7071daaefe6fca769c41513b610fa6d0
Marcel Petrick <mail@marcelpetrick.it>
2021-03-30 16:59:38 +0200

    videoPlayer (vlc with python for youtube): slightly adapted approach

--- lastfmLovedSongsToLocalYoutubePlayer/player.py
+++ lastfmLovedSongsToLocalYoutubePlayer/player.py
@@ -2,6 +2,7 @@
 #-----------------------------------------------
 import pafy
 import vlc
+from time import sleep
 
 url = "https://www.youtube.com/watch?v=bMt47wvK6u0"
 video = pafy.new(url)
@@ -18,8 +19,10 @@ player.play()
 print("after play")
 
 # prevent end of the run, so that there is time to run
-while True:
-     pass
+# OTOH: now the video-window does not react to any "close button" anymore
+sleep(6)
+while player.is_playing():
+    sleep(1)
 
 #---------------------------
 # outcome: works, but really awkward, there are no "controls" and nothing else to do
