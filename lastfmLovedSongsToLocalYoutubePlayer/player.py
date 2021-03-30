d2ca13ed6bcc9a85041669ad396bf7e07ba9894e
Marcel Petrick <mail@marcelpetrick.it>
2021-03-30 16:57:17 +0200

    videoPlayer (vlc with python for youtube): copy&pasted example from internet (mingled until it was runnable)

---
+++ lastfmLovedSongsToLocalYoutubePlayer/player.py
@@ -0,0 +1,26 @@
+# pip install youtube-dl pafy python-vlc
+#-----------------------------------------------
+import pafy
+import vlc
+
+url = "https://www.youtube.com/watch?v=bMt47wvK6u0"
+video = pafy.new(url)
+best = video.getbest()
+playUrl = best.url
+
+Instance = vlc.Instance()
+player = Instance.media_player_new()
+Media = Instance.media_new(playUrl)
+Media.get_mrl()
+player.set_media(Media)
+print("before play")
+player.play()
+print("after play")
+
+# prevent end of the run, so that there is time to run
+while True:
+     pass
+
+#---------------------------
+# outcome: works, but really awkward, there are no "controls" and nothing else to do
+# but: this player could be used to schedule plays of the found song-videos ..
