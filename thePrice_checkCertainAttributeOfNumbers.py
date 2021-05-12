55bd35c1a5f6c35455b004aac599024873fe17d4
Marcel Petrick <mail@marcelpetrick.it>
2021-05-12 16:27:15 +0200

    thePrice_checkCertainAttributeOfNumbers: hunk

---
+++ thePrice_checkCertainAttributeOfNumbers.py
@@ -0,0 +1,4 @@
+# Got an e-mail which announced some funding-camppaign.
+# But what is the number? Got me curious? Decided to find the first number which fits that description. But not manually ..
+# " All I can say is that the Kickstarter price is a prime, a safe prime, a supersingular prime, a Lucas prime, and an Eisenstein prime.
+# It is also a highly cototient number, a Keith number, a strictly non-palindromic number, and a Carol number. "
