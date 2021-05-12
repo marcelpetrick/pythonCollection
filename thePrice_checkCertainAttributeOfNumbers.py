4a5c78f1fd6bf10503d2976bb4f92b11a5506b62
Marcel Petrick <mail@marcelpetrick.it>
2021-05-12 16:35:08 +0200

    thePrice_checkCertainAttributeOfNumbers: added data for some sequences

--- thePrice_checkCertainAttributeOfNumbers.py
+++ thePrice_checkCertainAttributeOfNumbers.py
@@ -2,3 +2,13 @@
 # But what is the number? Got me curious? Decided to find the first number which fits that description. But not manually ..
 # " All I can say is that the Kickstarter price is a prime, a safe prime, a supersingular prime, a Lucas prime, and an Eisenstein prime.
 # It is also a highly cototient number, a Keith number, a strictly non-palindromic number, and a Carol number. "
+
+primes = []
+safePrimes = []
+superSingularPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 47, 59]
+lucasPrimes = []
+eisensteinPrimes = [2, 5, 11, 17, 23, 29, 41, 47, 53, 59, 71, 83, 89, 101, 107, 113, 131, 137, 149, 167, 173, 179, 191, 197, 227, 233, 239, 251, 257, 263, 269, 281, 293, 311, 317, 347, 353, 359, 383, 389, 401]
+cototienNumbers = [2, 4, 8, 23, 35, 47, 59, 63, 83, 89]
+ketihNumbers = [14, 19, 28, 47, 61, 75]
+strictlyNonPalindromicNumbers = []
+carolNumbers = [7, 47, 223, 959, 3967, 16127, 65023, 261119, 1046527, 4190207, 16769023, 67092479, 268402687, 1073676287, 4294836223, 17179607039, 68718952447, 274876858367, 1099509530623, 4398042316799, 17592177655807, 70368727400447, 281474943156223]
