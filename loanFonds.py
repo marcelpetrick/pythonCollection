c96386f71fdfd571096ec7f624d4c186ee8bf101
Marcel Petrick <mail@marcelpetrick.it>
2021-06-29 14:10:49 +0200

    simple loan vs. gains calculator: fixed the division bug; added the "runtime in months" as parameter; usable to get some quick overview

--- loanFonds.py
+++ loanFonds.py
@@ -2,17 +2,16 @@
 # I am not really convinced, but naively the question is: should the ETF not outgrow (quickly) the current low itnerest rates?
 # (83 for 1k for 12 months they say)
 
-def loanFondsPrinter(startAmount = 1000, monthlyRates = 83, etfGrowthAnuallyInPercent = 12):
+def loanFondsPrinter(startAmount = 1000, monthlyRates = 83, etfGrowthAnuallyInPercent = 12, monthsToRun = 12):
     # start at zero, then do the rates; first rate at 1
-    growthPerMonthFactor = (etfGrowthAnuallyInPercent // 12.0) / 100.0 + 1.0
+    growthPerMonthFactor = (etfGrowthAnuallyInPercent / 12.0) / 100.0 + 1.0
     amount = startAmount
     print("day 0 amount:", amount, "factor:", growthPerMonthFactor)
-    for month in range(1,12+1):
+    for month in range(1, monthsToRun+1):
         # to keep it conservative: first deduct, then increase the rest by the gain
         amount -= monthlyRates
         amount *= growthPerMonthFactor
         print("month", month, amount)
+    print("remainder:", amount)
 
-loanFondsPrinter(10000, 840, 20) # 508 remainder
-
-# todo make the amount of months interactive
+loanFondsPrinter(5000, 212, 10, 24) # 508 remainder
