85739b8d2268637c3d616864d3de7ece80a71240
Marcel Petrick <mail@marcelpetrick.it>
2021-07-02 15:39:45 +0200

    * fixFileNameReferenceInsideHeader: copy paste of the whitespace-fixer as base for development * fixFileNameReferenceInsideHeader: added test-input

---
+++ fixFileNameReferenceInsideHeader/testInputFile.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file	TODO
+ *
+ * Copyright (C) 2021 marcelpetrick.it
+ *
+ * marcelpetrick.it owns the sole copyright of the software. Under international
+ * copyright laws you (1) may not make a copy of this software except for the
+ * purposes of maintaining a single archive copy, (2) may not derive works
+ * herefrom, (3) may not distribute this work to others. These rights are
+ * provided for information clarification, other restrictions of rights may
+ * apply as well.
+ *
+ */
+ 
+class test
+{
+private:
+	 int foo{1337};
+public:
+	int getFoo() const;
+}
