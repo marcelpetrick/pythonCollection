416fb19ea528044abae347871b1ffee38a581435
Marcel Petrick <mpetrick@data-modul.com>
2021-07-02 16:19:27 +0200

    * fixFileNameReferenceInsideHeader: minor fix: take basename as filename instead of whole path!

--- fixFileNameReferenceInsideHeader/fixFileNameReferenceInsideHeader.py
+++ fixFileNameReferenceInsideHeader/fixFileNameReferenceInsideHeader.py
@@ -1,8 +1,10 @@
-# based on: <squashMultipleWhitespace.py>
+# @author mail@marcelpetrick.it
 
 def insertFilenameIntoHeader():
     # determine given parameter
     import sys
+    import ntpath
+
     if len(sys.argv) < 2:
         print("too less args: first param = filename")
         exit(1)
@@ -14,7 +16,8 @@ def insertFilenameIntoHeader():
     print("----------------------------- handling:", filename,"---------------") # remove later
 
     # replace with new line: " * @file	$filename$"
-    newLine = " * @file\t" + filename + "\n"
+    # but just use the filename, disacard the leading path!
+    newLine = " * @file    " + ntpath.basename(filename) + "\n" # also 4 spaces instead of tab
 
     # read everything in and store an edited version in buffer
     reading_file = open(filename, "r")
