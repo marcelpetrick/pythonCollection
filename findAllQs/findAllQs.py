e5dbbbe5ea877733bffbad20606c6f6091dba6a1
Marcel Petrick <mail@marcelpetrick.it>
2021-09-13 17:32:55 +0200

    findAllQs: bugfixing; results were missing; prefix Q_ was truncated

--- findAllQs/findAllQs.py
+++ findAllQs/findAllQs.py
@@ -17,9 +17,14 @@ def scanFileForQ(filename):
 
             # todo put all cropped substrings into a set
             hit = re.search('Q_(.+?)\(', normalized) # search for substring between Q_ and the opening bracket
+            # todo error: Q_NAMESPACE; is missing
             if (hit):
                 #print("hit:", hit.group(1))
-                results.add(hit.group(1))
+                results.add("Q_" + hit.group(1)) # prefix this again
+            else:
+                hit = re.search('Q_(.+?);', normalized) # between Q_ and ; - to find those mistakes like "Q_OBJECT;"
+                if (hit):
+                    results.add("Q_" + hit.group(1)) # prefix this again
 
     reading_file.close()
 
@@ -29,5 +34,22 @@ def scanFileForQ(filename):
 
 results = scanFileForQ("C:/vmSharedFolder/pedantic_output.txt")
 print("results:")
+print("---------------")
 for elem in results:
     print(elem)
+print("---------------")
+#---------------------------------------------------------------------------------------------------------
+# results:
+# ---------------
+# Q_NAMESPACE
+# Q_ENUM_NS
+# Q_OBJECT
+# Q_LOGGING_CATEGORY
+# Q_DECLARE_OPERATORS_FOR_FLAGS
+# Q_DECLARE_FLAGS
+# Q_DECLARE_LOGGING_CATEGORY
+# Q_DECLARE_METATYPE
+# Q_PROPERTY
+# ---------------
+#
+# Process finished with exit code 0
