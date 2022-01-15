acf37adeff4468108f184c95251bf455406cfa82
Marcel Petrick <mail@marcelpetrick.it>
2022-01-15 23:57:57 +0100

    autoTrans: started that idea

---
+++ AutoTrans/autoTrans.py
@@ -0,0 +1,7 @@
+# GPL v3
+# author: mail@marcelpetrick.it
+
+# idea: call like `python autoTrans.py input.ts output.ts` (later input is edited in place)
+# Replace all untranslated strings with the respective result from some online-translator.
+#
+# Steps? Read file linewise (tbd: may be a problem for wrapped strings ..), then take input and call whatever API (or webscrape?) and then replace it in the file.
