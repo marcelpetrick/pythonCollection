acbed553c4424c1bacedd6fa50212641e11e0077
Marcel Petrick <mail@marcelpetrick.it>
2022-01-16 00:05:30 +0100

    autoTrans: simple translation; but exception --> fixed; but now just single-strings work

--- AutoTrans/autoTrans.py
+++ AutoTrans/autoTrans.py
@@ -5,3 +5,22 @@
 # Replace all untranslated strings with the respective result from some online-translator.
 #
 # Steps? Read file linewise (tbd: may be a problem for wrapped strings ..), then take input and call whatever API (or webscrape?) and then replace it in the file.
+
+def simpleTest():
+    # use this to fix problem with no token ...
+    # `pip install googletrans==4.0.0-rc1`
+    from googletrans import Translator
+
+    translator = Translator()
+
+    translatedText = translator.translate('Überbackenes Weißbrot mit Wiener Schnitzel', src='de', dest='en')
+    # obviously the v4.0-rc1 has an issue with bulk translations: https://github.com/ssut/py-googletrans/issues/264
+
+    print(translatedText.text)
+
+    # original from the official googletrans-page
+    #translations = translator.translate(['The quick brown fox', 'jumps over', 'the lazy dog'], dest='ko')
+    #for translation in translations:
+    #    print(translation.origin, ' -> ', translation.text)
+# ------
+simpleTest()
