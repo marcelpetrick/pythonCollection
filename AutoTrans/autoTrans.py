1eb6e223800fe3a94b185f841948a0e0e5e7a205
Marcel Petrick <mail@marcelpetrick.it>
2022-01-17 15:49:03 +0100

    autoTrans: minor improvements; fixed docu

--- AutoTrans/autoTrans.py
+++ AutoTrans/autoTrans.py
@@ -4,23 +4,25 @@
 # idea: call like `python autoTrans.py input.ts output.ts` (later input is edited in place)
 # Replace all untranslated strings with the respective result from some online-translator.
 #
-# Steps? Read file linewise (tbd: may be a problem for wrapped strings ..), then take input and call whatever API (or webscrape?) and then replace it in the file.
+# Steps? Read file linewise (tbd: may pose a problem for wrapped strings ..),
+# then take input and call whatever API (or webscrape?) and then replace it in the file.
 
+# ------------------------------------------------------------------------------------------------------------
 def simpleTest():
-    # use this to fix problem with no token ...
+    # use this to fix problem with latest release which reports ".. no token .."
     # `pip install googletrans==4.0.0-rc1`
     from googletrans import Translator
 
     translator = Translator()
 
-    translatedText = translator.translate('Überbackenes Weißbrot mit Wiener Schnitzel', src='de', dest='en')
+    translation = translator.translate('Überbackenes Weißbrot mit Wiener Schnitzel und Tomaten.', src='de', dest='en')
     # obviously the v4.0-rc1 has an issue with bulk translations: https://github.com/ssut/py-googletrans/issues/264
 
-    print(translatedText.text)
+    print(translation.origin, " -> ", translation.text)
 
     # original from the official googletrans-page
     #translations = translator.translate(['The quick brown fox', 'jumps over', 'the lazy dog'], dest='ko')
     #for translation in translations:
     #    print(translation.origin, ' -> ', translation.text)
-# ------
+# ------------------------------------------------------------------------------------------------------------
 simpleTest()
