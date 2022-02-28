afff49ca97383689ae850d21919a199e38aad234
Marcel Petrick <mail@marcelpetrick.it>
2022-02-28 11:59:50 +0100

    AutoTrans: expanded the example ts-file heavily

--- AutoTrans/testing/helloworld.ts
+++ AutoTrans/testing/helloworld.ts
@@ -1,9 +1,26 @@
-<!DOCTYPE TS><TS>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
 <context>
     <name>QPushButton</name>
     <message>
         <source>Hello world!</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>My first dish.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>recipe name</name>
+    <message>
+        <source>white bread with butter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Green tea</source>
+        <translation>Grüner Tee</translation>
     </message>
 </context>
 </TS>
