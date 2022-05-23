16988da7d1aef9c6b35df06e5f49f383dd7ceb43
Marcel Petrick <mail@marcelpetrick.it>
2022-05-23 17:17:46 +0200

    powercycle: working script; failed first time after 6 cycles
    todo: timestamp; also count the number of cycles ..

--- PyUSB/powerCycle.sh
+++ PyUSB/powerCycle.sh
@@ -1,11 +1,13 @@
 #!/bin/bash    
 
-# todo cycle!
+# author: mail@marcelpetrick.it
+# date: 20220523
 
+# cycle
 while true; do
-    echo "------------------------------------------"
+    echo "--------------------------------------------------"
     # turn outlet 1 on
-    echo "turn on"
+    echo "* turn on *"
     sudo ./sispmctl.py -o 1
 
     # wait for 20 s until the X BO booted
@@ -13,7 +15,7 @@ while true; do
 
     # check the result of i2c bus
 
-    echo "--- ssh now ---"
+    echo "* ssh now *"
     # $ ssh root@192.168.100.21 cat /sys/devices/platform/soc@0/30800000.bus/30a30000.i2c/i2c-1/1-0041/{{firmware,kernel,protocol}_version,mode}
     # 0600.0000.b7cc.aa0a
     # 10.25.0d.00.01
@@ -21,13 +23,23 @@ while true; do
     # 5a.80:AP
     SSHRESULT=$(ssh root@192.168.100.21 cat /sys/devices/platform/soc@0/30800000.bus/30a30000.i2c/i2c-1/1-0041/{{firmware,kernel,protocol}_version,mode});
     echo "SSHRESULT=$SSHRESULT"
+    
+    # check the result; compare against first line "0600.0000.b7cc.aa0a"
+    if [[ $SSHRESULT = *"0600.0000.b7cc.aa0a"* ]]; then
+        echo "found version; driver works"
+    else
+        echo "version not found -> break!"
+        break
+    fi
+    
+    # wait
+    sleep 1s
 
     # turn outlet 1 off
-    echo "turn off"
+    echo "* turn off *"
     sudo ./sispmctl.py -f 1
 
-
-    echo "wait one more second"
+    echo "* wait one more second *"
     sleep 1s
 
 done # end of the while loop
