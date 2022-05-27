9390ea68187a14e488712b4ed5684dea8b51ec4f
Marcel Petrick <mail@marcelpetrick.it>
2022-05-27 15:06:37 +0200

    Update bindunbind.sh

--- PyUSB/bindunbind.sh
+++ PyUSB/bindunbind.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
+
+# print current firmware-version first
+echo "firmware information:"
+cat /sys/devices/platform/soc@0/30800000.bus/30a30000.i2c/i2c-1/1-0041/{{firmware,kernel,protocol}_version,mode} 
+
+# cycles
+echo "Will do the unbind/bind-cycles now. Check with \"dmesg -w\" the output."
 CYCLES=0
 while true ; do
-		CYCLES=$((CYCLES+1))
+	CYCLES=$((CYCLES+1))
         echo 1-0041 > /sys/bus/i2c/drivers/ili210x_i2c/unbind
         sleep 1
         echo 1-0041 > /sys/bus/i2c/drivers/ili210x_i2c/bind 
         sleep 1
         test -e /dev/input/touchscreen0 || break
         echo "ok: $CYCLES - $(date)"
-done 
+done
