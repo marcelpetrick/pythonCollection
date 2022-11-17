f2a628be81d8b21350f4d6e36b5a3d0f24ffba60
Marcel Petrick <mail@marcelpetrick.it>
2022-11-17 18:46:22 +0100

    dangernoodle: continued - the blue square can be moved now; lol

--- SnakeGameForKids/dangernoodle.py
+++ SnakeGameForKids/dangernoodle.py
@@ -4,12 +4,22 @@ def main():
     import pygame
 
     pygame.init()
-    dis = pygame.display.set_mode((640, 480))
+    boardWidth = 640
+    boardHeight = 480
+    dis = pygame.display.set_mode((boardWidth, boardHeight))
     pygame.display.update()
     pygame.display.set_caption('danger noodle')
 
     blue = (0, 0, 255)
-    red = (255, 0, 0)
+    white = (255, 255, 255)
+
+    xPos = boardWidth / 2
+    yPos = boardHeight / 2
+
+    xDiff = 0
+    yDiff = 0
+
+    clock = pygame.time.Clock()
 
     game_over = False
     while not game_over:
@@ -20,8 +30,27 @@ def main():
                 print("closed via X")
                 game_over = True
 
-            pygame.draw.rect(dis, blue, [200, 150, 10, 10])
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    xDiff = -10
+                    yDiff = 0
+                elif event.key == pygame.K_RIGHT:
+                    xDiff = 10
+                    yDiff = 0
+                elif event.key == pygame.K_UP:
+                    xDiff = 0
+                    yDiff = -10
+                elif event.key == pygame.K_DOWN:
+                    xDiff = 0
+                    yDiff = 10
+
+            xPos += xDiff
+            yPos += yDiff
+            dis.fill(white)
+            pygame.draw.rect(dis, blue, [xPos, yPos, 10, 10])
             pygame.display.update()
+
+            clock.tick(25)
 
     pygame.quit()
     quit()
