806126f62840eb7e4605cd8454f11c485132bec6
Marcel Petrick <mpetrick@data-modul.com>
2023-07-13 17:47:11 +0200

    autoTrans: added comments

--- AutoTrans/autoTrans_vs.py
+++ AutoTrans/autoTrans_vs.py
@@ -1,9 +1,14 @@
 import sys
 import xml.etree.ElementTree as ET
-import shutil
 
 
 def replace_first_lines(file_path):
+    """
+    Replaces the first two lines of a file with the XML declaration and DOCTYPE.
+
+    :param file_path: The path to the file to modify
+    :type file_path: str
+    """
     with open(file_path, 'r+', encoding='utf-8') as file:
         lines = file.readlines()
         lines[0] = '<?xml version="1.0" encoding="utf-8"?>\n'
@@ -15,15 +20,33 @@ def replace_first_lines(file_path):
 
 
 def translateString(input):
+    """
+    Translates a given string. The actual implementation should be replaced
+    with your desired translation logic.
+
+    :param input: The string to translate
+    :type input: str
+    :return: The translated string
+    :rtype: str
+    """
     # Implement your translation logic here
     # This is just a placeholder
     translation = "Translated: " + input
     return translation
 
+
 def transform_ts_file(ts_file_path):
+    """
+    Transforms a .ts file by translating all 'unfinished' messages.
+    The translated messages replace the original messages in the .ts file.
+
+    :param ts_file_path: The path to the .ts file to transform
+    :type ts_file_path: str
+    """
     tree = ET.parse(ts_file_path)
     root = tree.getroot()
 
+    # Iterate over all 'message' elements in the XML tree
     for message in root.iter('message'):
         translation = message.find('translation')
         if translation is not None and translation.attrib.get('type') == 'unfinished':
@@ -43,13 +66,19 @@ def transform_ts_file(ts_file_path):
 
     print("TS file transformed successfully.")
 
+
 def main():
+    """
+    The main entry point of the script. It checks if a file path was given as
+    command line argument and if so, calls the function to transform the .ts file.
+    """
     if len(sys.argv) < 2:
         print("Usage: python script.py <ts_file_path>")
         return
 
     ts_file_path = sys.argv[1]
     transform_ts_file(ts_file_path)
+
 
 if __name__ == "__main__":
     main()
