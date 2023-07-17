4c229ca4bc363b89cfd13fa483ab29fbd281ec32
Marcel Petrick <mpetrick@data-modul.com>
2023-07-17 16:00:02 +0200

    autoTrans: more docu, added the langage-selection as input-parameter

--- AutoTrans/autoTrans_vs.py
+++ AutoTrans/autoTrans_vs.py
@@ -19,14 +19,33 @@ def replace_first_lines(file_path):
         file.writelines(lines)
         file.truncate()
 
-def translateString(input, fromLang, toLang):
+
+def translateString(input: str, fromLang: str, toLang: str) -> str:
+    """
+    Translate a string from one language to another.
+
+    This function uses Google's translation service, but could be modified to use another service like DeepL or Yandex.
+    The translation process is timed, and the duration is printed alongside the original and translated strings and the source and target languages.
+
+    Parameters:
+    input (str): The string to translate.
+    fromLang (str): The ISO 639-1 code of the language to translate from.
+    toLang (str): The ISO 639-1 code of the language to translate to.
+
+    Returns:
+    str: The translated string.
+
+    Example:
+    >>> translateString("hello world", "en", "es")
+    'hola mundo'
+    """
     startTime = time.time()
     output = translators.google(input, fromLang, toLang) # or deepl or yandex
     print(f"translateString: {time.time() - startTime}s : {input} -> {output} ({fromLang} -> {toLang})")
 
     return output
 
-def transform_ts_file(ts_file_path):
+def transform_ts_file(ts_file_path, sourceLanguage, targetLanguage):
     """
     Transforms a .ts file by translating all 'unfinished' messages.
     The translated messages replace the original messages in the .ts file.
@@ -37,14 +56,14 @@ def transform_ts_file(ts_file_path):
     tree = ET.parse(ts_file_path)
     root = tree.getroot()
 
-    fromLang, toLang = 'en', 'de' # todo make this dependent on the given parameters, maybe read from ts-file
+    #sourceLanguage, targetLanguage = 'en', 'de' # todo make this dependent on the given parameters, maybe read from ts-file
 
     # Iterate over all 'message' elements in the XML tree
     for message in root.iter('message'):
         translation = message.find('translation')
         if translation is not None and translation.attrib.get('type') == 'unfinished':
             source_text = message.find('source').text
-            translated_text = translateString(source_text, fromLang, toLang)
+            translated_text = translateString(source_text, sourceLanguage, targetLanguage)
             translation.text = translated_text
             del translation.attrib['type']
 
@@ -65,16 +84,28 @@ def main():
     The main entry point of the script. It checks if a file path was given as
     command line argument and if so, calls the function to transform the .ts file.
     """
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <ts_file_path>")
+    if len(sys.argv) < 4:
+        print("Usage: python script.py <ts_file_path> sourceLanguage targetLanguage")
         return
 
     ts_file_path = sys.argv[1]
-    transform_ts_file(ts_file_path)
-
+    startTime = time.time()
+    transform_ts_file(ts_file_path, sys.argv[2], sys.argv[3])
+    print(f"Whole execution took {time.time() - startTime}s.")
 
 if __name__ == "__main__":
     main()
 
 # test call:
-#  python autoTrans_vs.py testing/helloworld.ts
+#  python autoTrans_vs.py testing/helloworld.ts en de
+
+# another example:
+#
+# (venv) [mpetrick@marcel-precision3551 AutoTrans]$  python autoTrans_vs.py testing/helloworld.ts en cn
+# Using Germany server backend.
+# translateString: 1.3896245956420898s : Hello world! -> 你好世界！ (en -> cn)
+# translateString: 1.9492523670196533s : My first dish. -> 我的第一道菜。 (en -> cn)
+# translateString: 2.112003803253174s : white bread with butter -> 白面包和黄油 (en -> cn)
+# TS file transformed successfully.
+# Whole execution took 5.453961610794067s.
+# (venv) [mpetrick@marcel-precision3551 AutoTrans]$
