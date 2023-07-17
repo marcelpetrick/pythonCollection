984ac02a1e7107b7f40fd87545d74f780d1709fe
Marcel Petrick <mpetrick@data-modul.com>
2023-07-17 14:51:28 +0200

    autoTrans: translator-backend-test: currently not successful with `translators`

---
+++ AutoTrans/translators_test.py
@@ -0,0 +1,23 @@
+# for the package - newer versions are broken!
+# pip install translators==5.3.1
+
+# for the backend:
+# pacman -S nodejs npm
+
+import time
+import translators
+
+def translateString(input, fromLang, toLang):
+    startTime = time.time()
+    result = translators.google(input, fromLang, toLang) # or deepl or yandex
+    print(f"translateString: translation took {time.time() - startTime}s")
+
+    return result
+
+print(translateString("Kuh Haus Maus Klaus", "de", "en"))
+print(translateString("Das ist eine Pizza", "de", "en"))
+print(translateString("dritter Call", "de", "en"))
+
+# Result: most of the time the three calls are only translated until the second request: last one fails due to
+# requests.exceptions.HTTPError: 429 Client Error:  for url: https://www2.deepl.com/jsonrpc?method=LMT_handle_jobs
+# maybe using a delay would help: else: other backend? or: doing batch-translations?
