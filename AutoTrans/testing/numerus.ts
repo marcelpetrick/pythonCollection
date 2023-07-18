ab9886c269fc49ef4f2ea91796c6443cd62c9ae3
Marcel Petrick <mail@marcelpetrick.it>
2023-07-18 17:26:22 +0200

    auto_trans: added numerus form for testing

---
+++ AutoTrans/testing/numerus.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>QPushButton</name>
+    <message numerus="yes">
+        <source>+ %n min</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
